384414f0068309f447bcdc381dbce96de19811bf
derek <derekc150@gmail.com>
2020-03-07 13:59:17 -0800

    attempting to fix event registration requests

--- handlers/registration.js
+++ handlers/registration.js
@@ -1,7 +1,13 @@
 'use strict';
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
-const helpers = require('./helpers')
+const helpers = require('./helpers');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey("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");
+//send grid acc:
+//user: ubcbiztech
+//pass: ubcbiztech123
+//key: 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
 
 module.exports.create = async (event, ctx, callback) => {
 
@@ -28,25 +34,52 @@ module.exports.create = async (event, ctx, callback) => {
   }
   const id = parseInt(data.id, 10);
   const eventID = data.eventID;
+  let eventName = "";
   let registrationStatus = data.registrationStatus;
 
   // Check if the event is full
-  if (registrationStatus === 'registered'){
+  if (registrationStatus === 'registered') {
 
     const eventParams = {
       Key: { id: eventID },
       TableName: 'biztechEvents' + process.env.ENVIRONMENT
     }
-    
-    await docClient.get(eventParams).promise()
-    .then(async(event) => {
-      const counts = await helpers.getEventCounts(eventID)
 
-      if (counts.registeredCount >= event.Item.capac){
-        registrationStatus = 'waitlist'
+    await docClient.get(eventParams).promise()
+      .then(async (event) => {
+        console.log(event);
+        const counts = await helpers.getEventCounts(eventID);
+        eventName = event.ename;
+
+        if (counts.registeredCount >= event.Item.capac) {
+          registrationStatus = 'waitlist'
+        }
+      })
+  }
+
+  //email confirmation 
+  userParams = {
+    Key: { id: id },
+    TableName: 'biztechUsers' + process.env.ENVIRONMENT
+  }
+  await docClient.get(userParams).promise()
+    .then(async (user) => {
+      const email = await user.email;
+      const userName = await user.fname;
+      if (eventName != "") {
+        const msg = {
+          to: 'derekc150@gmail.com',
+          from: 'dev@ubcbiztech.com',
+          subject: 'Biztech ' + eventName + ' Receipt',
+          text: 'Hi ' + userName + '\n\n' + 'You have been ' + registrationStatus + ' for ' + eventName + '.',
+        }
+        console.log(msg);
+        if (msg) {
+          sgMail.send(msg);
+        }
       }
     })
-  }
+
 
   const updateObject = { registrationStatus };
   console.log(updateObject)
@@ -65,33 +98,33 @@ module.exports.create = async (event, ctx, callback) => {
     TableName: 'biztechRegistration' + process.env.ENVIRONMENT,
     ExpressionAttributeValues: expressionAttributeValues,
     UpdateExpression: updateExpression,
-    ReturnValues:"UPDATED_NEW"
+    ReturnValues: "UPDATED_NEW"
   };
 
   // call dynamoDb
   await docClient.update(params).promise()
-  .then(result => {
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify({
-        message: 'Update succeeded',
-        registrationStatus
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({
+          message: 'Update succeeded',
+          registrationStatus
         })
-    };
-    callback(null, response)
-  })
-  .catch(error => {
-    console.error(error);
-    const response = {
-    statusCode: 500,
-    body: error
-    };
-    callback(null, response)
-  });
+      };
+      callback(null, response)
+    })
+    .catch(error => {
+      console.error(error);
+      const response = {
+        statusCode: 500,
+        body: error
+      };
+      callback(null, response)
+    });
 };
 
 // Return list of entries with the matching id
@@ -124,7 +157,7 @@ module.exports.queryStudent = async (event, ctx, callback) => {
         body: JSON.stringify({
           size: data.length,
           data: data
-          }, null, 2)
+        }, null, 2)
       };
       callback(null, response);
     })
@@ -153,25 +186,25 @@ module.exports.scanEvent = async (event, ctx, callback) => {
   };
 
   await docClient.scan(params).promise()
-  .then(result => {
-    console.log('Scan success.');
-    const data = result.Items;
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify({
-        size: data.length,
-        data: data
+    .then(result => {
+      console.log('Scan success.');
+      const data = result.Items;
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({
+          size: data.length,
+          data: data
         }, null, 2)
-    };
-    callback(null, response);
-  })
-  .catch(error => {
-    console.error(error);
-    callback(new Error('Unable to scan registration table.'));
-    return;
-  });
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Unable to scan registration table.'));
+      return;
+    });
 }
