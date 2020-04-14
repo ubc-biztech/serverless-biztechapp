7a0e78745ec05883e42770525b6fcd26442fa91f
derek <derekc150@gmail.com>
2020-04-13 20:39:10 -0700

    removed html template and used drag/drop dynamic template with variables instead

--- handlers/registration.js
+++ handlers/registration.js
@@ -58,68 +58,24 @@ module.exports.create = async (event, ctx, callback) => {
     Key: { id: id },
     TableName: 'biztechUsers' + process.env.ENVIRONMENT
   }
+
   await docClient.get(userParams).promise()
     .then(async (user) => {
       console.log(user);
-      const email = "derekc150@gmail.com";
-      //const email = await user.Item.email;
+      const userEmail = await user.Item.email;
       const userName = await user.Item.fname;
       if (eventName != undefined) {
-        let htmlTemplate = `
-          <!DOCTYPE html>
-          <html>
-          <body>
 
-          <div name="container" style="width:100%; background-color: #C0C0C0">
-            <div name="innerDiv" style="width: 50%; background-color: #FFFFFF; margin: 0 auto">
-              <div name="header" style="display: inline-block; padding-top:20px; padding-bottom: 20px; width: 100%;height: 110px; background-color: #DCDCDC">
-                <img src="https://i.imgur.com/F40JOUo.png" style="width:87px; height:115px; vertical-align:middle; padding-left:30px">
-                <div style="display: inline-block">
-                 <div style="display: inline-block; margin-left: 20px">
-                      <h1 style="display: inline-block; color: #565656; font-family: arial; margin-right: 5px">UBC</h3>
-                      <h1 style="display: inline-block; color: #7AD040; font-family: arial">BIZTECH</h3>
-                 </div>
-             </div>
-          </div>
-          <div name="body" style="width: 100%; margin-right:40px">
-            <div>
-                <p style="display: inline-block; margin-right:5px; margin-left: 20px">Hi</p><p style="display: inline-block">${userName}</p>
-                <br>
-                <p style="display: inline-block; margin-right:3px; margin-left: 20px">You have been </p> <p style="display: inline-block; margin-right: 3px">${registrationStatus}</p><p style="display: inline-block; margin-right: 4px">for UBC BizTech's </p><p style="display: inline-block; margin-right: 4px">${eventName}</p><p style="display: inline-block">event.</p>
-                <br>
-                <p style="margin-left: 20px">We look forward to hosting you!</p>
-                <p></p>
-            </div>            
-        </div>
-        <div name="footer" style="display: inline-block; padding-bottom: 10px; width: 100%;height: 60px; background-color: #A9A9A9">
-          <div style="padding-left:20px">
-            <p style="font-size:12px;display: inline-block;">info@ubcbiztech.com,</p>
-            <p style="font-size:12px;display: inline-block;">
-            <span>Vancouver, British Columbia V6T 1Z2</span>
-            </p>
-          </div>
-          <p style="font-size:12px; padding:0; margin:0">
-          <a href="{{{unsubscribe}}}" target="_blank" style="font-family:sans-serif;text-decoration:none; margin-left:20px">
-            Unsubscribe
-          </a>
-          -
-          <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="font-family:sans-serif;text-decoration:none;">
-            Unsubscribe Preferences
-          </a>
-          </p>
-          </div>
-        </div>
-    </div>
-</div>
-
-          </body>
-          </html>
-        `
         const msg = {
-          to: email,
-          from: 'info@ubcbiztech.com',
-          subject: 'BizTech ' + eventName + ' Receipt',
-          html: htmlTemplate,
+          to: userEmail,
+          from: "info@ubcbiztech.com",
+          templateId: "d-99da9013c9a04ef293e10f0d73e9b49c",
+          dynamic_template_data: {
+            subject: "BizTech " + eventName + "Receipt",
+            name: userName,
+            registrationStatus: registrationStatus,
+            eventName: eventName
+          }
         }
         if (msg) {
           await sgMail.send(msg);
