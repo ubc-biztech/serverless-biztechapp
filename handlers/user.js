cc355b8a0e0530cdf511153e995e7bfa8039e199
derek <derekc150@gmail.com>
2020-05-03 19:08:31 -0700

    refactored serverless.yml and added get all users

--- handlers/user.js
+++ handlers/user.js
@@ -14,19 +14,19 @@ module.exports.create = async (event, ctx, callback) => {
   const id = parseInt(data.id, 10);
 
   const params = {
-      Item: {
-          id,
-          fname: data.fname,
-          lname: data.lname,
-          email: data.email,
-          faculty: data.faculty,
-          year: data.year,
-          gender: data.gender,
-          diet: data.diet,
-          createdAt: timestamp,
-          updatedAt: timestamp
-      },
-      TableName: 'biztechUsers' + process.env.ENVIRONMENT
+    Item: {
+      id,
+      fname: data.fname,
+      lname: data.lname,
+      email: data.email,
+      faculty: data.faculty,
+      year: data.year,
+      gender: data.gender,
+      diet: data.diet,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    },
+    TableName: 'biztechUsers' + process.env.ENVIRONMENT
   };
 
   await docClient.put(params).promise()
@@ -49,15 +49,15 @@ module.exports.get = async (event, ctx, callback) => {
   const id = parseInt(queryString.id, 10);
 
   const params = {
-      Key: {
-        id
-      },
-      TableName: 'biztechUsers' + process.env.ENVIRONMENT
+    Key: {
+      id
+    },
+    TableName: 'biztechUsers' + process.env.ENVIRONMENT
   };
 
   await docClient.get(params).promise()
     .then(result => {
-      if (result.Item == null){
+      if (result.Item == null) {
         const response = helpers.createResponse(404, 'User not found.')
         callback(null, response)
       } else {
@@ -87,7 +87,7 @@ module.exports.update = async (event, ctx, callback) => {
   };
 
   await docClient.get(params).promise()
-    .then(async(result) => {
+    .then(async (result) => {
       if (!helpers.isEmpty(result))
         callback(null, await helpers.updateDB(id, data, 'biztechUsers'));
       else {
@@ -102,3 +102,25 @@ module.exports.update = async (event, ctx, callback) => {
     })
 
 };
+
+module.exports.getAll = async (event, ctx, callback) => {
+  const params = {
+    TableName: 'biztechUsers' + process.env.ENVIRONMENT
+  }
+
+  await docClient.scan(params).promise()
+    .then(async (result) => {
+      if (result.Items == null) {
+        const response = helpers.createResponse(404, 'User not found.');
+        callback(null, response);
+      } else {
+        const response = helpers.createResponse(200, result.Items);
+        callback(null, response);
+      }
+    })
+    .catch(async (error) => {
+      console.error(error);
+      const response = helpers.createResponse(502, error);
+      callback(null, response);
+    })
+}
