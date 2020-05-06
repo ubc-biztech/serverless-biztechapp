2b35b30f96b2946524fc5551258c7e4663696d57
Ian Mah <ian.mah@hootsuite.com>
2020-05-05 23:46:17 -0700

    users/invite -> invitecodes/

--- handlers/user.js
+++ handlers/user.js
@@ -2,8 +2,6 @@
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 const helpers = require('./helpers');
-const crypto = require('crypto');
-const email = require('../utils/email')
 
 module.exports.create = async (event, ctx, callback) => {
 
@@ -128,42 +126,4 @@ module.exports.update = async (event, ctx, callback) => {
       callback(null, response);
     })
 
-};
-
-module.exports.invite = async (event, ctx, callback) => {
-  const data = JSON.parse(event.body);
-  if (!data.hasOwnProperty('email')) {
-    return helpers.inputError('Email not specified.', data);
-  }
-
-  const id = crypto.randomBytes(20).toString('hex')
-
-  const params = {
-    Item: {
-      id,
-      email: data.email
-    },
-    TableName: 'inviteCodes' + process.env.ENVIRONMENT
-  }
-
-  await docClient.put(params).promise()
-    .then(success => {
-        const msg = {
-          to: data.email,
-          templateId: "d-198cfc5057914538af105ef469f51217",
-          dynamic_template_data: {
-            url: 'https://app.ubcbiztech.com/invite/'+id // TODO: Fix url format based on frontend implementation
-          }
-        }
-        return email.send(msg)
-    })
-    .then(success => {
-      const response = helpers.createResponse(200, 'Invite code created & sent to ' + data.email)
-      callback(null, response)
-    })
-    .catch(error => {
-      console.error(error);
-      const response = helpers.createResponse(502, error)
-      callback(null, response);
-    });
 };
