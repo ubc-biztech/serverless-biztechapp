877ded4d50d7185a2e535e1965020651a9fe2380
Ian <ianmmah@gmail.com>
2020-05-08 20:40:26 -0700

    Redirect dev emails to dev@ubcbiztech.com

--- utils/email.js
+++ utils/email.js
@@ -7,17 +7,19 @@ module.exports.send = (msg) => {
         // default from address
         msg.from = "info@ubcbiztech.com"
     }
-    
+
     if (process.env.ENVIRONMENT === 'PROD') {
         // Production emails are okay to send
         return sgMail.send(msg)
     } else {
         // Dev/staging emails should only be sent to ubcbiztech emails
         // This is to reduce accidental test emails being sent to random addresses
-        // and prevent us from being marked as spam
+        // and prevent us from being marked as spam. Redirect to dev@ubcbiztech.com
         if (msg.to.includes('@ubcbiztech.com')) {
+            return sgMail.send(msg)
+        } else {
+            msg.to = 'dev@ubcbiztech.com'
             return sgMail.send(msg)
         }
     }
-    return Promise.reject('Did not send email')
 }
