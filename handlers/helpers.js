edc816c952e946663c3c7716da219f0ff6289124
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-06-09 00:45:54 -0700

    fix helper functions

--- handlers/helpers.js
+++ handlers/helpers.js
@@ -1,5 +1,4 @@
 const AWS = require("aws-sdk");
-const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports = {
   isEmpty: function (obj) {
@@ -43,6 +42,8 @@ module.exports = {
    * @param {String} tableName - Name of table to call batchGet
    */
   batchGet: function (batch, tableName) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
     const batchRequestParams = {
       RequestItems: {
         [tableName]: {
@@ -89,6 +90,7 @@ module.exports = {
    * @param {String} table - name of table, ie 'biztechUsers'
    */
   updateDB: async function (id, obj, table) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
     var updateExpression = "set ";
     var expressionAttributeValues = {};
 
@@ -139,6 +141,7 @@ module.exports = {
    * @return {registeredCount checkedInCount waitlistCount}
    */
   getEventCounts: async function (eventID) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
     const params = {
       TableName: "biztechRegistration" + process.env.ENVIRONMENT,
       FilterExpression: "eventID = :query",
