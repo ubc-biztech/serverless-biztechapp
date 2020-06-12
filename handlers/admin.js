8f8b0801843c2ecf3485a8c462637cdcd45df37d
Ian Mah <ian.mah@hootsuite.com>
2020-06-12 00:19:46 -0700

    Properly set user as admin

--- handlers/admin.js
+++ handlers/admin.js
@@ -10,27 +10,27 @@ module.exports.create = async (event, ctx, callback) => {
     const { email } = identity;
   
     //verify this was called by an email with @ubcbiztech.com
-    if(!email.substring(email.indexOf("@") + 1, email.length) === 'ubcbiztech.com') {
+    if(!(email.substring(email.indexOf("@") + 1, email.length) === 'ubcbiztech.com')) {
       const response = helpers.createResponse(403, 'Could not assign user as an admin');
       return callback(null, response);
+    } else {
+        const params = {
+          GroupName: 'admin',
+          UserPoolId: 'us-west-2_w0R176hhp',
+          Username: identity['cognito:username']
+        };
+      
+        await cognito.adminAddUserToGroup(params).promise()
+          .then(() => {
+            const response = helpers.createResponse(200, 'Success');
+            callback(null, response);
+          })
+          .catch(error => {
+            console.error(error);
+            const response = helpers.createResponse(502, 'Error setting user as admin');
+            callback(null, response);
+          })
     }
-  
-    const params = {
-      GroupName: 'admin',
-      UserPoolId: 'us-west-2_w0R176hhp',
-      Username: identity['cognito:username']
-    };
-  
-    await cognito.adminAddUserToGroup(params).promise()
-      .then(() => {
-        const response = helpers.createResponse(200, 'Success');
-        callback(null, response);
-      })
-      .catch(error => {
-        console.error(error);
-        const response = helpers.createResponse(502, 'Error setting user as admin');
-        callback(null, response);
-      })
   
   }
   
