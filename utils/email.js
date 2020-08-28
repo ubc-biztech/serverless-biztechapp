c5c69da107acc8b7afe78f42d416e8ae7efeb59a
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-08-27 22:34:22 -0700

    invite code endpoints

--- utils/email.js
+++ utils/email.js
@@ -3,23 +3,35 @@ sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 // Returns a promise
 module.exports.send = (msg) => {
-    if (!msg.from) {
-        // default from address
-        msg.from = "info@ubcbiztech.com"
+
+  if (!msg.from) {
+
+    // default from address
+    msg.from = 'info@ubcbiztech.com';
+
+  }
+
+  if (process.env.ENVIRONMENT === 'PROD') {
+
+    // Production emails are okay to send
+    return sgMail.send(msg);
+
+  } else {
+
+    // Dev/staging emails should only be sent to ubcbiztech emails
+    // This is to reduce accidental test emails being sent to random addresses
+    // and prevent us from being marked as spam. Redirect to dev@ubcbiztech.com
+    if (msg.to.includes('@ubcbiztech.com')) {
+
+      return sgMail.send(msg);
+
+    } else {
+
+      msg.to = 'dev@ubcbiztech.com';
+      return sgMail.send(msg);
+
     }
 
-    if (process.env.ENVIRONMENT === 'PROD') {
-        // Production emails are okay to send
-        return sgMail.send(msg)
-    } else {
-        // Dev/staging emails should only be sent to ubcbiztech emails
-        // This is to reduce accidental test emails being sent to random addresses
-        // and prevent us from being marked as spam. Redirect to dev@ubcbiztech.com
-        if (msg.to.includes('@ubcbiztech.com')) {
-            return sgMail.send(msg)
-        } else {
-            msg.to = 'dev@ubcbiztech.com'
-            return sgMail.send(msg)
-        }
-    }
-}
+  }
+
+};
