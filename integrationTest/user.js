b2f865622ea7af87c3f1e80a12839bd8e64ff774
Jacques Chen <jacqueschen1@gmail.com>
2020-09-12 22:39:46 -0700

    add descriptions

--- integrationTest/user.js
+++ integrationTest/user.js
@@ -15,10 +15,13 @@ describe('user integration', function () {
     }
   };
 
-  it('user GET doesn\'t exist returns 404', async () => {
-    return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(404);
+  describe('user/{id} GET setup', function () {
+
+    it('user GET doesn\'t exist returns 404', async () => {
+      return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(404);
+      });
     });
   });
 
@@ -34,20 +37,23 @@ describe('user integration', function () {
       diet: 'vegan',
       favedEventsArray: ['someEvent', 'bluePrint'],
     })
-  }
+  };
 
-  it('user POST returns 201', async () => {
-    return helpers.invokeLambda('userCreate', JSON.stringify(userCreatePayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(201);
-      expect(body.message).to.equal('Created!');
+  describe('user/ POST', function () {
+
+    it('user POST returns 201', async () => {
+      return helpers.invokeLambda('userCreate', JSON.stringify(userCreatePayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(201);
+        expect(body.message).to.equal('Created!');
+      });
     });
-  });
 
-  it('user POST already exists returns 409', async () => {
-    return helpers.invokeLambda('userCreate', JSON.stringify(userCreatePayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(409);
+    it('user POST already exists returns 409', async () => {
+      return helpers.invokeLambda('userCreate', JSON.stringify(userCreatePayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(409);
+      });
     });
   });
 
@@ -67,80 +73,86 @@ describe('user integration', function () {
     body: JSON.stringify(userPatchBody)
   }
 
-  it('user PATCH on user that exists returns 200', async() => {
-    return helpers.invokeLambda('userUpdate', JSON.stringify(userPatchPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(200);
+  describe('user/{id} PATCH', function () {
+
+    it('user PATCH on user that exists returns 200', async() => {
+      return helpers.invokeLambda('userUpdate', JSON.stringify(userPatchPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(200);
+      });
+    });
+
+    it('user favouriteEvent PATCH returns 200', async () => {
+      const payload = {
+        pathParameters: {
+          id: -40,
+        },
+        body: JSON.stringify({
+          isFavourite: true,
+          eventID: 'randomEvent',
+        }),
+      }
+
+      return helpers.invokeLambda('userFavouriteEvent', JSON.stringify(payload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(200);
+        expect(body).to.equal('Favouriting event \'randomEvent\' success.')
+      });
+    });
+
+    it('user unfavouriteEvent PATCH returns 200', async () => {
+      const payload = {
+        pathParameters: {
+          id: -40,
+        },
+        body: JSON.stringify({
+          isFavourite: false,
+          eventID: 'bluePrint',
+        }),
+      }
+
+      return helpers.invokeLambda('userFavouriteEvent', JSON.stringify(payload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(200);
+        expect(body).to.equal('Unfavouriting event \'bluePrint\' success.')
+      });
+    });
+
+    it('user GET exists returns 200 and check PATCH success', async () => {
+      return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(200);
+        // check that update succeeded
+        expect(body.fname).to.equal(userPatchBody.fname);
+        expect(body.year).to.equal(userPatchBody.year);
+        expect(body.gender).to.equal(userPatchBody.gender);
+        expect(body.favedEventsID).to.contain('randomEvent');
+        expect(body.favedEventsID).to.contain('someEvent');
+      });
     });
   });
 
-  it('user favouriteEvent PATCH returns 200', async () => {
-    const payload = {
-      pathParameters: {
-        id: -40,
-      },
-      body: JSON.stringify({
-        isFavourite: true,
-        eventID: 'randomEvent',
-      }),
-    }
+  describe('user/{id} DELETE and wrapup', function () {
 
-    return helpers.invokeLambda('userFavouriteEvent', JSON.stringify(payload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(200);
-      expect(body).to.equal('Favouriting event \'randomEvent\' success.')
+    it('user DELETE returns 200', async () => {
+      return helpers.invokeLambda('userDelete', JSON.stringify(defaultPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(200);
+      });
     });
-  });
 
-  it('user unfavouriteEvent PATCH returns 200', async () => {
-    const payload = {
-      pathParameters: {
-        id: -40,
-      },
-      body: JSON.stringify({
-        isFavourite: false,
-        eventID: 'bluePrint',
-      }),
-    }
-
-    return helpers.invokeLambda('userFavouriteEvent', JSON.stringify(payload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(200);
-      expect(body).to.equal('Unfavouriting event \'bluePrint\' success.')
+    it('user GET returns 404 to check DELETE worked', async () => {
+      return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(404);
+      });
     });
-  });
 
-  it('user GET exists returns 200 and check PATCH success', async () => {
-    return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(200);
-      // check that update succeeded
-      expect(body.fname).to.equal(userPatchBody.fname);
-      expect(body.year).to.equal(userPatchBody.year);
-      expect(body.gender).to.equal(userPatchBody.gender);
-      expect(body.favedEventsID).to.contain('randomEvent');
-      expect(body.favedEventsID).to.contain('someEvent');
-    });
-  });
-
-  it('user DELETE returns 200', async () => {
-    return helpers.invokeLambda('userDelete', JSON.stringify(defaultPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(200);
-    });
-  });
-
-  it('user GET returns 404 to check DELETE worked', async () => {
-    return helpers.invokeLambda('userGet', JSON.stringify(defaultPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(404);
-    });
-  });
-
-  it('user PATCH on user that does not exist returns 404', async () => {
-    return helpers.invokeLambda('userGet', JSON.stringify(userPatchPayload))
-    .then(([statusCode, body]) => {
-      expect(statusCode).to.equal(404);
+    it('user PATCH on user that does not exist returns 404', async () => {
+      return helpers.invokeLambda('userGet', JSON.stringify(userPatchPayload))
+      .then(([statusCode, body]) => {
+        expect(statusCode).to.equal(404);
+      });
     });
   });
 });
