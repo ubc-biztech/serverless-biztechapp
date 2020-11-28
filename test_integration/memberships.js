edcf791a74b6a3c01036c8810f39d956c25de9b1
huangsamantha <samanthaannhuang@gmail.com>
2020-11-27 16:39:23 -0800

    Check return body in integration test

--- test_integration/memberships.js
+++ test_integration/memberships.js
@@ -13,9 +13,12 @@ describe('memberships integration', function () {
     it('memberships GET returns 200 on success', async () => {
 
       return helpers.invokeLambda('membershipsGetAll', '')
-        .then(([statusCode]) => {
+        .then(([statusCode, body]) => {
 
           expect(statusCode).to.equal(200);
+          expect(body).to.not.be.empty;
+          expect(body[0]).to.have.property('id');
+          expect(body[0]).to.have.property('email');
 
         });
 
