e838676acdbc9e12adedfa55286ccb10617474fc
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-11-30 00:20:35 -0800

    allow tests to be run from different directories

--- scripts/run_utests.sh
+++ scripts/run_utests.sh
@@ -2,12 +2,16 @@
 
 # By default, use "all"
 collection=${1:-all}
+function=${1:-all}
 
 if [ $collection == all ]
 then # run unit tests for all collections
     sls invoke test --compilers js:babel-core/register --path services/*/test --root services/*
-else # run unit tests for a specific function
+elif [ $function == all ]
+then # run unit tests for all functions in a collection
     sls invoke test --compilers js:babel-core/register --path services/${collection}/test --root services/${collection}
+else # run unit tests for a specific function
+    cd ./services/${collection} && sls invoke test --compilers js:babel-core/register --function ${function}
 fi
 
 # Notes:
