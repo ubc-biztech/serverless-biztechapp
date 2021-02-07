5ca37f7e5712602c6bc09124e08709d224399ddb
Resocram <marcoser2000@gmail.com>
2021-02-06 17:48:19 -0800

    Fixed CI problems

--- services/registrations/test_integration/registration.js
+++ services/registrations/test_integration/registration.js
@@ -175,7 +175,7 @@ describe('registration integration', function () {
             }
             if (entry['eventID;year'] === `${INTEGRATION_TEST_PERSISTENT_EVENT_ID_2};${INTEGRATION_TEST_PERSISTENT_YEAR_2}`) {
 
-              expect(entry.registrationStatus).to.equal('waitlist');
+              expect(entry.registrationStatus).to.equal('registered');
 
             }
 
