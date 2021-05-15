894e1b591a530333357cdd9605b8cb50776a04aa
Merissa Li <merissajli@gmail.com>
2021-05-15 13:51:37 -0700

    Defined biztechMembers2022 table & renamed all 'memberships' to 'members'

--- services/members/handler.js
+++ services/members/handler.js
@@ -1,17 +1,17 @@
 import helpers from '../../lib/handlerHelpers';
 import db from '../../lib/db';
-const { MEMBERSHIPS_TABLE } = require('../../constants/tables');
+const { MEMBERS2022_TABLE } = require('../../constants/tables');
 
 export const getAll = async(event, ctx, callback) => {
 
   try {
 
     // scan the table
-    const memberships = await db.scan(MEMBERSHIPS_TABLE);
+    const members = await db.scan(MEMBERS2022_TABLE);
 
     // re-organize the response
     let response = {};
-    if (memberships !== null) response = helpers.createResponse(200, memberships);
+    if (members !== null) response = helpers.createResponse(200, members);
 
     // return the response object
     callback(null, response);
---
+++ services/members/test_integration/memberships.js
@@ -0,0 +1,24 @@
+"use strict";
+import chai from "chai";
+const expect = chai.expect;
+
+import helpers from "../../../lib/testHelpers";
+
+const SERVICE = "memberss";
+
+describe("memberss integration", function () {
+  this.timeout(10000);
+
+  describe("members/ GET tests", function () {
+    it("members GET returns 200 on success", async () => {
+      return helpers
+        .invokeLambda(SERVICE, "membersGetAll", "")
+        .then(([statusCode, body]) => {
+          expect(statusCode).to.equal(200);
+          expect(body).to.not.be.empty;
+          expect(body[0]).to.have.property("id");
+          expect(body[0]).to.have.property("email");
+        });
+    });
+  });
+});
