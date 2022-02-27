7b65ef0ffa65c9b17b94a29dafa93cbef7744b6d
Merissa Li <merissajli@gmail.com>
2022-02-27 02:01:39 -0800

    additional test metadata

--- services/memberships/handler.js
+++ services/memberships/handler.js
@@ -1,13 +1,13 @@
-import helpers from '../../lib/handlerHelpers';
-import db from '../../lib/db';
-const stripe = require('stripe')('sk_test_51KOxOlBAxwbCreS7JRQtvZCnCgLmn8tjK7WPHDGjpw0s4vfVHLwbcrZZvQLmd5cY7zKRIsfj3pnEDDHTy3G81Tuf00v9ygIBrC');
+import helpers from "../../lib/handlerHelpers";
+import db from "../../lib/db";
+const stripe = require("stripe")(
+  "sk_test_51KOxOlBAxwbCreS7JRQtvZCnCgLmn8tjK7WPHDGjpw0s4vfVHLwbcrZZvQLmd5cY7zKRIsfj3pnEDDHTy3G81Tuf00v9ygIBrC"
+);
 // development endpoint secret - switch to live secret key in production
-const endpointSecret = 'whsec_TYSFr29HQ4bIPu649lgkxOrlPjrDOe2l';
-const { MEMBERSHIPS_TABLE } = require('../../constants/tables');
+const endpointSecret = "whsec_TYSFr29HQ4bIPu649lgkxOrlPjrDOe2l";
+const { MEMBERSHIPS_TABLE } = require("../../constants/tables");
 export const getAll = async (event, ctx, callback) => {
-
   try {
-
     // scan the table
     const memberships = await db.scan(MEMBERSHIPS_TABLE);
 
@@ -19,46 +19,34 @@ export const getAll = async (event, ctx, callback) => {
     // return the response object
     callback(null, response);
     return null;
-
   } catch (err) {
-
     callback(null, err);
     return null;
-
   }
-
 };
 
-export const webhook = async(event, ctx, callback) => {
-
-  const sig = event.headers['Stripe-Signature'];
+export const webhook = async (event, ctx, callback) => {
+  const sig = event.headers["Stripe-Signature"];
   let eventData;
   console.log(event.body);
 
   // Stripe returns an error if verification fails
   try {
-
     eventData = stripe.webhooks.constructEvent(event.body, sig, endpointSecret);
-
-  } catch(err) {
-
+  } catch (err) {
     return helpers.createResponse(400, {
-      message: `Webhook Error: ${err.message}`
+      message: `Webhook Error: ${err.message}`,
     });
-
   }
 
   // Handle the checkout.session.completed event
-  if (eventData.type == 'checkout.session.completed') {
-
+  if (eventData.type == "checkout.session.completed") {
     console.log(eventData.data);
-
   }
 
   let response = helpers.createResponse(200, {});
   callback(null, response);
   return null;
-
 };
 
 export const config = {
@@ -67,17 +55,16 @@ export const config = {
   },
 };
 
-export const payment = async(event, ctx, callback) => {
-
+export const payment = async (event, ctx, callback) => {
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ["card"],
     line_items: [
       {
         price_data: {
-          currency: 'CAD',
+          currency: "CAD",
           product_data: {
-            name: 'BizTech Membership',
-            images: ['https://imgur.com/TRiZYtG.png'],
+            name: "BizTech Membership",
+            images: ["https://imgur.com/TRiZYtG.png"],
           },
           unit_amount: 500,
         },
@@ -86,18 +73,23 @@ export const payment = async(event, ctx, callback) => {
     ],
     metadata: {
       // event.body
-      order_id: '12345',
-      student_number: '1234567',
-      first_name: 'John',
-      last_name: 'Cena',
-      test_test: 'test',
+      order_id: "12345",
+      student_number: "1234567",
+      first_name: "John",
+      last_name: "Cena",
+      test_test: "test",
+      id: ctx.id,
+      id2: ctx.body.id,
+      education0: ctx.education,
+      education1: ctx.body.education,
+      education: event.education,
+      email: event.body.email,
     },
-    mode: 'payment',
-    success_url: 'https://app.ubcbiztech.com/signup/success',
-    cancel_url: 'https://facebook.com',
+    mode: "payment",
+    success_url: "https://app.ubcbiztech.com/signup/success",
+    cancel_url: "https://facebook.com",
   });
   let response = helpers.createResponse(200, session.url);
   callback(null, response);
   return null;
-
 };
