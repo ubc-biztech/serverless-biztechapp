3741d544897651d290289afc315449297ac4cc0e
Allan Tan <allan.zf.tan@gmail.com>
2023-07-07 20:28:16 -0700

    script

---
+++ scripts/resetMemberships.js
@@ -0,0 +1,27 @@
+import db from "../lib/db.js";
+import docClient from "../lib/docClient.js";
+import { USERS_TABLE } from "../constants/tables.js";
+
+const members = await db.scan(USERS_TABLE, {
+    FilterExpression: 'isMember <> :isMemberValue',
+    ExpressionAttributeValues: {
+        ':isMemberValue': false
+    }
+});
+
+const reset = () => {
+    members.forEach((member) => {
+        const paramsUpdate = {
+            TableName: USERS_TABLE,
+            Key: { id: member.id },
+            UpdateExpression: 'set isMember = :isMember',
+            ExpressionAttributeValues: {
+                ":isMember": false,
+            }
+        };
+        docClient.update(paramsUpdate, (errUpdate) => {
+            if (errUpdate) console.error(errUpdate);
+        });
+    })    
+}
+// reset()
