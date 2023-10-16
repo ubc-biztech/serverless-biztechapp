3b746724d7390eaaf57b0cd67432c7bfbf951bcd
dennis <dennis34@students.cs.ubc.ca>
2023-10-15 17:28:04 -0700

    fixed naming

--- scripts/findmembers.js
+++ scripts/findmembers.js
@@ -2,13 +2,13 @@ import * as dotenv from "dotenv";
 import AWS from "aws-sdk";
 
 dotenv.config({
-  path: "../.env"
+    path: "../.env"
 });
 
 const awsConfig = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "us-west-2",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "us-west-2",
 };
 
 const cognitoISP = new AWS.CognitoIdentityServiceProvider(awsConfig);
@@ -16,52 +16,52 @@ const cognitoISP = new AWS.CognitoIdentityServiceProvider(awsConfig);
 const userPoolId = "us-west-2_w0R176hhp";
 const filter = "username ^= \"Google\"";
 
-const getCountsByEmail = async (emails) => {
-  const counts = [];
+const filterRegUsers = async (emails) => {
+    const counts = [];
 
-  for (let i = 0; i < emails.length; i++) {
-    const email = emails[i];
-    try {
-      const data = await cognitoISP.listUsers({
-        UserPoolId: userPoolId,
-        Filter: `email = "${email}"`,
-      }).promise();
+    for (let i = 0; i < emails.length; i++) {
+        const email = emails[i];
+        try {
+            const data = await cognitoISP.listUsers({
+                UserPoolId: userPoolId,
+                Filter: `email = "${email}"`,
+            }).promise();
 
-      if (data.Users.length === 1) {
-        counts.push(email);
-      }
-    } catch (error) {
-      console.error("Error", error);
+            if (data.Users.length === 1) {
+                counts.push(email);
+            }
+        } catch (error) {
+            console.error("Error", error);
+        }
     }
-  }
 
-  return counts;
+    return counts;
 };
 
 cognitoISP.listUsers({
-  UserPoolId: userPoolId,
-  Filter: filter
+    UserPoolId: userPoolId,
+    Filter: filter
 }, (err, data) => {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    const threeMonthsAgo = new Date();
-    threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+    if (err) {
+        console.log("Error", err);
+    } else {
+        const threeMonthsAgo = new Date();
+        threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
 
-    const recentUsers = data.Users.filter(user =>
-      new Date(user.UserCreateDate) >= threeMonthsAgo
-    );
-    const result = [];
-    recentUsers.forEach(user => {
-      user.Attributes.forEach(attribute => {
-        if (attribute.Name === "email") {
-          result.push(attribute.Value);
-        }
-      });
-    });
-    getCountsByEmail(result).then(counts => {
-      console.log("Endangered Emails", counts);
-    });
-  }
+        const recentUsers = data.Users.filter(user =>
+            new Date(user.UserCreateDate) >= threeMonthsAgo
+        );
+        const result = [];
+        recentUsers.forEach(user => {
+            user.Attributes.forEach(attribute => {
+                if (attribute.Name === "email") {
+                    result.push(attribute.Value);
+                }
+            });
+        });
+        filterRegUsers(result).then(counts => {
+            console.log("Endangered Emails", counts);
+        });
+    }
 });
 
