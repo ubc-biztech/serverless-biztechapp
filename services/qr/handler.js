ca02ba64a596023e03702877c6e0274db3b6ce6a
steven huang <114288160+stevenh17@users.noreply.github.com>
2023-12-11 17:59:07 -0800

    changes based on PR comments

--- services/qr/handler.js
+++ services/qr/handler.js
@@ -2,13 +2,9 @@ import docClient from "../../lib/docClient";
 
 import registrationHelpers from "./helpers";
 import helpers from "../../lib/handlerHelpers";
-import {
-  isEmpty
-} from "../../lib/utils";
+import { isEmpty } from "../../lib/utils";
 import db from "../../lib/db";
-import {
-  EVENTS_TABLE, QRS_TABLE
-} from "../../constants/tables";
+import { EVENTS_TABLE, QRS_TABLE } from "../../constants/tables";
 
 /*
   Returns Status Code 200 when QR code is scanned successfully
@@ -64,20 +60,23 @@ export const post = async (event, ctx, callback) => {
       .qrScanPostHelper(data, data.email)
       .then(async (res) => {
         if (res.hasOwnProperty("errorMessage")) {
-          const response_fail = helpers.createResponse(403, {
-            message: "ERROR: " + res.errorMessage,
-            response: res
-          });
-          callback(null, response_fail);
-          return response_fail;
+          if (
+            res.errorMessage === "Team scan would result in negative points"
+          ) {
+            const response_fail = helpers.createResponse(406, {
+              message: "ERROR: " + res.errorMessage,
+              response: res
+            });
+
+            callback(null, response_fail);
+            return response_fail;
+          }
         } else {
           try {
             await registrationHelpers.logQRScan(data.qrCodeID, data.email);
-            console.log("hey success!!");
           } catch (logErr) {
             console.error("Error logging QR scan:", logErr);
           }
-          console.log("yoyo im here");
           const response_success = helpers.createResponse(200, {
             message: "Successfully scanned QR code.",
             response: res
@@ -100,8 +99,7 @@ export const post = async (event, ctx, callback) => {
 
 export const get = async (event, ctx, callback) => {
   try {
-    const qrs = await db.scan(QRS_TABLE, {
-    });
+    const qrs = await db.scan(QRS_TABLE, {});
     const response = helpers.createResponse(200, qrs);
     callback(null, response);
     return response;
@@ -121,9 +119,7 @@ export const getOne = async (event, ctx, callback) => {
       !event.pathParameters.year
     )
       throw helpers.missingPathParamResponse("id", "event", "year");
-    const {
-      id, eventID, year
-    } = event.pathParameters;
+    const { id, eventID, year } = event.pathParameters;
     const eventIDAndYear = eventID + ";" + year;
     const qr = await db.scan(id, QRS_TABLE, {
       "eventID;year": eventIDAndYear
@@ -214,9 +210,7 @@ export const update = async (event, ctx, callback) => {
       !event.pathParameters.year
     )
       throw helpers.missingPathParamResponse("id", "event", "year");
-    const {
-      id, eventID, year
-    } = event.pathParameters;
+    const { id, eventID, year } = event.pathParameters;
     const eventIDAndYear = eventID + ";" + year;
 
     const existingQR = await db.getOne(id, QRS_TABLE, {
@@ -274,9 +268,7 @@ export const del = async (event, ctx, callback) => {
       !event.pathParameters.year
     )
       throw helpers.missingPathParamResponse("id", "event", "year");
-    const {
-      id, eventID, year
-    } = event.pathParameters;
+    const { id, eventID, year } = event.pathParameters;
     const eventIDAndYear = eventID + ";" + year;
 
     const existingQR = await db.getOne(id, QRS_TABLE, {
