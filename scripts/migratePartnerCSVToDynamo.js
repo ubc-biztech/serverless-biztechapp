c5f26a5e44cfd524cf04f519e6455b97d8995db6
Allan Tan <allan.zf.tan@gmail.com>
2024-01-02 16:39:20 -0800

    script finished

--- scripts/migratePartnerCSVToDynamo.js
+++ scripts/migratePartnerCSVToDynamo.js
@@ -1,9 +1,10 @@
 import db from "../lib/db.js";
 import docClient from "../lib/docClient.js";
 import {
-  EVENTS_TABLE
+  USER_REGISTRATIONS_TABLE
 } from "../constants/tables.js";
 import * as fs from "fs";
+import csv from "csv-parser";
 
 let registrationData = {
   email: "",
@@ -19,168 +20,182 @@ let registrationData = {
     companyName: "",
     role: ""
   },
-  dynamicResponses: {}
-}
-
-const readCSV = () => {
-  const csvFilePath = "scripts/test.csv";
-  const csvData = [];
-  let csvHeader;
-
-  fs.readFile(csvFilePath, "utf-8", (err, data) => {
-    if (err) {
-      console.error(`Error reading CSV file: ${err.message}`);
-      return;
-    }
-
-    const lines = data.split(/\r?\n/);
-    csvHeader = lines[0].split(",");
-
-    for (let i = 1; i < lines.length; i++) {
-      const row = lines[i].split(",");
-      csvData.push(row);
-    }
-
-    console.log("CSV Header:", csvHeader);
-    console.log("CSV Data:", csvData);
-  });
-  return [csvHeader, csvData];
+  dynamicResponses: {
+  }
 };
 
-const prepareData = (questions) => {
+let googleFormToDevForm = {
+  "What is your confirmed role?": "e79bd73c-9b27-424c-af0d-ed5b2b170675",
+  "LinkedIn URL": "96d4e60e-6832-4f9f-b127-ecf734cbb05f",
+  "Headshot": "4026ffed-9f12-4519-8e53-11720b5cfaae",
+  "Do you have any dietary restrictions?": "12596ae4-98ad-4ed0-9b62-1d40aecb2e4a",
+  "Please indicate any accessibility/mobility needs below": "75f50fe7-e2b3-4e56-84db-a8e923756c23",
+  "What is your area of expertise? Example : UX Research, Venture Capital, Project Management": "647f5bd4-d8b8-47fc-a016-b00c6ab8afd7",
+  "Is there anything else that you'd like us to be aware of?": "dfe5b2e4-28f8-4f0f-b934-1f2a79a16060",
+  "[For Panelists & Workshop Hosts] Brief biography of current and/or past work experience, professional interests, etc. (3-4 sentences)": "9917f8fe-29c8-4a8b-8124-afc72425ba8c"
+};
+
+const prepareData = (csvData, id, year) => {
+  let data = csvData[1];
   let registrations = [];
-  // for 
-}
+
+  for (let d of data) {
+    let newRegistration = JSON.parse(JSON.stringify(registrationData));
+    const [fname, lname] = getFirstLastName(d["Full Name"]);
+
+    newRegistration.eventID = id;
+    newRegistration.year = year;
+    newRegistration.fname = fname;
+    newRegistration.email = d["Email Address"];
+    newRegistration.basicInformation.fname = fname;
+    newRegistration.basicInformation.lname = lname;
+    newRegistration.basicInformation.gender = d["Preferred Pronouns"];
+    newRegistration.basicInformation.companyName = d["Company Name"];
+    newRegistration.basicInformation.role = d["Role at Company"];
+    Object.entries(googleFormToDevForm).forEach(([key, value]) => {
+      newRegistration.dynamicResponses[value] = cleanString(d[key]);
+    });
+    registrations.push(newRegistration);
+  }
+  return registrations;
+};
+
+const getFirstLastName = (fullName) => {
+  const arr = fullName.split(" ");
+  if (arr.length <= 2) {
+    return arr;
+  }
+  const lastIndex = arr.length - 1;
+  const firstPart = arr.slice(0, lastIndex);
+  const lastElement = arr[lastIndex];
+
+  return [firstPart.join(" "), lastElement];
+};
+
+const cleanString = (inputString) => {
+  // Replace \r, \n, and other whitespace characters with an empty string
+  const cleanedString = inputString.replace(/[\r\n\s]+/g, " ");
+
+  return cleanedString;
+};
+
+const readCSV = async (csvFilePath) => {
+  return new Promise((resolve, reject) => {
+    const csvData = [];
+    let csvHeader;
+
+    const processCSV = (filePath) => {
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on("headers", (headers) => {
+          // Trim spaces from headers
+          csvHeader = headers.map(header => header.trim());
+        })
+        .on("data", (row) => {
+          // Trim spaces from key values in the data
+          const cleanedRow = Object.fromEntries(
+            Object.entries(row).map(([key, value]) => [key.trim(), value.trim()])
+          );
+          csvData.push(cleanedRow);
+        })
+        .on("end", () => {
+          // console.log(csvData);
+          resolve([csvHeader, csvData]);
+        })
+        .on("error", (error) => {
+          console.error(`Error reading CSV file: ${error.message}`);
+          reject(error);
+        });
+    };
+
+    processCSV(csvFilePath);
+  });
+};
+
+
+const removeDefaultKeys = (data) => {
+  const formResponse = data;
+  const ignoreKeys = ["eventID", "year", "email"];
+
+  Object.keys(formResponse).forEach(function (key) {
+    if (ignoreKeys.includes(key)) delete formResponse[key];
+  });
+  return formResponse;
+};
+
+const writeToDB = async (
+  registrationStatus,
+  data,
+  email,
+  eventIDAndYear,
+  createNew
+) => {
+  try {
+    const formResponse = removeDefaultKeys(data);
+    console.log("------");
+    console.log(data);
+    console.log("-----=-");
+
+    const updateObject = {
+      ...formResponse
+    };
+
+    let conditionExpression =
+      "attribute_exists(id) and attribute_exists(#eventIDYear)";
+    // if we are creating a new object, the condition expression needs to be different
+    if (createNew)
+      conditionExpression =
+        "attribute_not_exists(id) and attribute_not_exists(#eventIDYear)";
+
+    // construct the update expressions
+    const {
+      updateExpression,
+      expressionAttributeValues,
+      expressionAttributeNames
+    } = db.createUpdateExpression(updateObject);
+
+    // Because biztechRegistration table has a sort key, we cannot use helpers.updateDB()
+    let params = {
+      Key: {
+        id: email,
+        ["eventID;year"]: eventIDAndYear
+      },
+      TableName:
+        USER_REGISTRATIONS_TABLE +
+        (process.env.ENVIRONMENT ? process.env.ENVIRONMENT : ""),
+      ExpressionAttributeValues: expressionAttributeValues,
+      ExpressionAttributeNames: {
+        ...expressionAttributeNames,
+        "#eventIDYear": "eventID;year"
+      },
+      UpdateExpression: updateExpression,
+      ReturnValues: "UPDATED_NEW",
+      ConditionExpression: conditionExpression
+    };
+    console.log(params);
+    // do the magic
+    await docClient.update(params).promise();
+    let message = `User with email ${email} successfully registered (through update) to status '${registrationStatus}'!`;
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const migrate = async (id, year) => {
-  const existingEvent = await db.getOne(id, EVENTS_TABLE, {
-    year
-  });
-  
-  const data = readCSV();
-  const preppedData = prepareData(existingEvent.partnerRegistrationQuestions);
+  // Call this to obtain the question IDs and manually put into the googleFormToDevForm object
+  // const existingEvent = await db.getOne(id, EVENTS_TABLE, {
+  //   year
+  // });
+  // console.log(existingEvent.partnerRegistrationQuestions);
+
+  const data = await readCSV("./scripts/test.csv");
+  const registrations = prepareData(data, id, year);
+  let proms = [];
+  for (let r of registrations) {
+    proms.push(writeToDB("registered", r, r.email, id + ";" + year, true));
+  }
+  // await Promise.all(registrations);
 };
 
-
-/*
-this is data
-[registrations] {
-[registrations]   email: "asdfsdf@gmailc.om",
-[registrations]   fname: "asdfsafd",
-[registrations]   eventID: "script",
-[registrations]   year: 2023,
-[registrations]   registrationStatus: "registered",
-[registrations]   isPartner: true,
-[registrations]   basicInformation: {
-[registrations]     fname: "asdfsafd",
-[registrations]     lname: "asdf",
-[registrations]     gender: "He/Him/His",
-[registrations]     companyName: "Splunk",
-[registrations]     role: "SWE"
-[registrations]   },
-[registrations]   dynamicResponses: {
-[registrations]     "e3ae9339-a5e7-453f-bc98-e87e7bb461e6": "Sponsor, Keynote Speaker",
-[registrations]     "55bf0df1-0952-42b1-96a1-1b94accd42e2": "linkedin.com",
-[registrations]     "edafe5e4-e08b-43d2-aeb3-6bc45bbb4a73": "n/a",
-[registrations]     "c2c104bd-1035-4b4c-927d-eb1ccc0cfbc9": "swe",
-[registrations]     "51d7b71e-babe-454b-ae8a-ec1cb9c1549f": "n/a",
-[registrations]     "9944eb54-1b37-4613-aab2-20f47bf05c23": "asdf"
-[registrations]   }
-[registrations] }
-[registrations] {
-[registrations]   email: "asdfsdf@gmailc.om",
-[registrations]   fname: "asdfsafd",
-[registrations]   eventID: "script",
-[registrations]   year: 2023,
-[registrations]   registrationStatus: "registered",
-[registrations]   isPartner: true,
-[registrations]   basicInformation: {
-[registrations]     fname: "asdfsafd",
-[registrations]     lname: "asdf",
-[registrations]     gender: "He/Him/His",
-[registrations]     companyName: "Splunk",
-[registrations]     role: "SWE"
-[registrations]   },
-[registrations]   dynamicResponses: {
-[registrations]     "e3ae9339-a5e7-453f-bc98-e87e7bb461e6": "Sponsor, Keynote Speaker",
-[registrations]     "55bf0df1-0952-42b1-96a1-1b94accd42e2": "linkedin.com",
-[registrations]     "edafe5e4-e08b-43d2-aeb3-6bc45bbb4a73": "n/a",
-[registrations]     "c2c104bd-1035-4b4c-927d-eb1ccc0cfbc9": "swe",
-[registrations]     "51d7b71e-babe-454b-ae8a-ec1cb9c1549f": "n/a",
-[registrations]     "9944eb54-1b37-4613-aab2-20f47bf05c23": "asdf",
-                    "1babaeb4-97e9-452b-b64b-b127c0b826cc": "https://drive.google.com/file/d/1zuIIAddBRweftZYrUyYWAT8koOrKNBuT/view?usp=drivesdk"
-[registrations]   }
-[registrations] }
-[registrations] CloudWatch debugging purposes
-[registrations] {
-[registrations]   year: 2023,
-[registrations]   endDate: "2024-05-29T22:44:00.000Z",
-[registrations]   isPublished: false,
-[registrations]   partnerRegistrationQuestions: [
-[registrations]     {
-[registrations]       questionImageUrl: "",
-[registrations]       label: "What is your confirmed role?",
-[registrations]       questionId: "e3ae9339-a5e7-453f-bc98-e87e7bb461e6",
-[registrations]       type: "CHECKBOX",
-[registrations]       choices: "Sponsor,Keynote Speaker,Workshop Host,Networking Delegate,Panelist",
-[registrations]       required: true
-[registrations]     },
-[registrations]     {
-[registrations]       charLimit: 200,
-[registrations]       questionId: "55bf0df1-0952-42b1-96a1-1b94accd42e2",
-[registrations]       questionImageUrl: "",
-[registrations]       label: "Linkedin URL",
-[registrations]       type: "TEXT",
-[registrations]       choices: "",
-[registrations]       isSkillsQuestion: true,
-[registrations]       required: true
-[registrations]     },
-[registrations]     {
-[registrations]       label: "Please indicate any accesibility ",
-[registrations]       charLimit: 200,
-[registrations]       questionId: "edafe5e4-e08b-43d2-aeb3-6bc45bbb4a73",
-[registrations]       type: "TEXT",
-[registrations]       choices: "",
-[registrations]       required: false
-[registrations]     },
-[registrations]     {
-[registrations]       label: "Area of expertise",
-[registrations]       charLimit: 100,
-[registrations]       questionId: "c2c104bd-1035-4b4c-927d-eb1ccc0cfbc9",
-[registrations]       type: "TEXT",
-[registrations]       choices: "",
-[registrations]       required: true
-[registrations]     },
-[registrations]     {
-[registrations]       label: "Brief biography of current",
-[registrations]       charLimit: 100,
-[registrations]       questionId: "51d7b71e-babe-454b-ae8a-ec1cb9c1549f",
-[registrations]       type: "TEXT",
-[registrations]       choices: "",
-[registrations]       required: false
-[registrations]     },
-[registrations]     {
-[registrations]       label: "Beware of aything else?",
-[registrations]       questionId: "9944eb54-1b37-4613-aab2-20f47bf05c23",
-[registrations]       type: "TEXT",
-[registrations]       choices: "",
-[registrations]       required: false
-[registrations]     }
-[registrations]   ],
-[registrations]   description: "asdf",
-[registrations]   feedback: "",
-[registrations]   createdAt: 1703980079735,
-[registrations]   ename: "test-script",
-[registrations]   capac: 123,
-[registrations]   elocation: "nonappbased",
-[registrations]   imageUrl: "https://i.natgeofe.com/n/566ed88f-7ee4-4a57-be2e-aa312a5f65a1/capybara_4x3.jpg",
-[registrations]   id: "script",
-[registrations]   deadline: "2024-03-06T23:44:00.000Z",
-[registrations]   partnerDescription: "asdfasdf",
-[registrations]   startDate: "2023-12-30T23:44:37.504Z",
-[registrations]   pricing: { members: 0 },
-[registrations]   isApplicationBased: false,
-[registrations]   updatedAt: 1703980079735,
-[registrations]   registrationQuestions: []
-[registrations] }
-*/
+migrate("test-script", 2024);
