a2465584c8f9abdd9af4f74106ff11e469f19906
dennis <dennis34@students.cs.ubc.ca>
2024-01-22 20:25:55 -0800

    added discount code promotions

--- services/payments/handler.js
+++ services/payments/handler.js
@@ -347,7 +347,8 @@ export const payment = async (event, ctx, callback) => {
       mode: "payment",
       success_url: data.success_url,
       cancel_url: data.cancel_url,
-      expires_at: Math.round(new Date().getTime() / 1000) + 1800
+      expires_at: Math.round(new Date().getTime() / 1000) + 1800,
+      allow_promotion_codes: true
     });
 
     if (data.paymentType === "Event") {
