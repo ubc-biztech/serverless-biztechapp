99a8be798a3b943090ab768ab228cab88359cf46
Allan Tan <92118801+AllanT102@users.noreply.github.com>
2024-03-20 07:07:08 -0700

    updated email flow for application process (#349)

--- services/registrations/EmailService/templates/dynamicQRTemplates.js
+++ services/registrations/EmailService/templates/dynamicQRTemplates.js
@@ -27,6 +27,36 @@ export const getDefaultQRTemplate = (emailParams) => {
     </div>`;
 };
 
+export const getDefaultApplicationTemplate = (emailParams) => {
+  const {
+    fname, ename, registrationStatus, logoBase64
+  } = emailParams;
+
+  return `<div style="font-size: 15px; text-align: left;">
+      <div>
+          <p>Hello ${fname},</p>
+          <p>Your application status for UBC BizTech's ${ename} event is: <b>${registrationStatus}</b>. You can check your application status in your
+          personal <a href="https://app.ubcbiztech.com/companion">companion</a>.</p>
+          <p>Please reach out to our Experiences Team Lead at <a href="mailto:karen@ubcbiztech.com">karen@ubcbiztech.com</a> if this is a mistake.</p>
+      </div>
+      <img src="${logoBase64}" width="40" height="40" alt="BizTech Logo">
+      <br>
+      <div style="font-size: 8px;">
+          <div>
+              <p>UBC BizTech • 445-2053 Main Mall • Vancouver, BC V6T 1Z2</p>
+          </div>
+          <div>
+              <p>Copyright © 2022 UBC BizTech</p>
+          </div>
+      </div>
+      <div>
+          <u><a href="https://www.facebook.com/BizTechUBC">Facebook</a></u>
+          <u><a href="https://www.instagram.com/ubcbiztech/">Instagram</a></u>
+          <u><a href="https://www.linkedin.com/company/ubcbiztech/mycompany/">LinkedIn</a></u>
+      </div>
+      </div>`;
+};
+
 export const getRegisteredQRTemplate = (emailParams) => {
   const {
     fname, ename, logoBase64
