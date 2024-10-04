a8e80a26890a674749bb0144c35c8a363a7fbcd5
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-10-04 00:18:18 -0700

    Set up stickers YAML and required libraries

--- services/stickers/handler.js
+++ services/stickers/handler.js
@@ -1,70 +1,43 @@
 import helpers from "../../lib/handlerHelpers";
 import db from "../../lib/db";
-import {
-  isEmpty
-} from "../../lib/utils";
-import {
-  STICKERS_TABLE
-} from "../../constants/tables";
+import { isEmpty } from "../../lib/utils";
+import { STICKERS_TABLE } from "../../constants/tables";
+import AWS from "@aws-sdk/client-apigatewaymanagementapi";
 
-export const getAll = async(event, ctx, callback) => {
-  try {
-    // scan the table
-    const stickers = await db.scan(STICKERS_TABLE);
+export const connectionHandler = async (event, context, cb) => {};
 
-    // re-organize the response
-    let response = {
-    };
-    if (stickers !== null) response = helpers.createResponse(200, stickers);
-
-    // return the response object
-    callback(null, response);
-    return null;
-  } catch (err) {
-    callback(null, err);
-    return null;
-  }
+export const defaultHandler = async (event, context, cb) => {
+  const domain = event.requestContext.domainName;
+  const stage = event.requestContext.stage;
+  const connectionId = event.requestContext.connectionId;
+  const callbackUrlForAWS = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
+  await sendMessageToClient(callbackUrlForAWS, connectionId, event);
+  return {
+    statusCode: 200
+  };
 };
-export const create = async(event, ctx, callback) => {
-  try {
-    const data = JSON.parse(event.body);
 
-    helpers.checkPayloadProps(data, {
-      id: {
-        required: true,
-        type: "string"
+export const stickerHandler = async (event, context, cb) => {};
+
+export const scoreHandler = async (event, context, cb) => {};
+
+const sendMessageToClient = async (url, connectionId, payload) =>
+  new Promise((resolve, reject) => {
+    const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
+      apiVersion: "2018-11-29",
+      endpoint: url
+    });
+    apigatewaymanagementapi.postToConnection(
+      {
+        ConnectionId: connectionId, // connectionId of the receiving ws-client
+        Data: JSON.stringify(payload)
       },
-      name: {
-        required: true,
-        type: "string"
-      },
-      url: {
-        required: true,
-        type: "string"
+      (err, data) => {
+        if (err) {
+          console.log("err is", err);
+          reject(err);
+        }
+        resolve(data);
       }
-    });
-
-    const existingSticker = await db.getOne(data.id, STICKERS_TABLE);
-    if (!isEmpty(existingSticker)) throw helpers.duplicateResponse("sticker id", data);
-
-    const item = {
-      id: data.id,
-      name: data.name,
-      url: data.url
-    };
-    const res = await db.create(item, STICKERS_TABLE);
-
-    const response = helpers.createResponse(201, {
-      message: `Created sticker with id ${data.id}!`,
-      response: res,
-      item
-    });
-
-    callback(null, response);
-    return null;
-  } catch (err) {
-    console.error(err);
-    callback(null, err);
-    return null;
-  }
-};
+    );
+  });
