8d0ea7c1c75abf301117428bdbc97b4527ce7b0c
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-10-07 14:08:39 -0700

    Feat: refactor helpers + add comments

--- services/stickers/handler.js
+++ services/stickers/handler.js
@@ -1,4 +1,11 @@
-import helpers from "./helpers";
+import {
+  checkPayloadProps,
+  deleteConnection,
+  fetchState,
+  notifyVoters,
+  sendMessage,
+  updateSocket
+} from "./helpers";
 import db from "../../lib/db";
 import {
   STICKERS_TABLE,
@@ -9,6 +16,13 @@ import {
 import { DeleteCommand, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import docClient from "../../lib/docClient";
 
+/**
+ * Connection handler
+ *
+ * Persists connection in DB, if not synced, by default connection will be set
+ * to voter role
+ *
+ */
 export const connectHandler = async (event, ctx, callback) => {
   const connectionID = event.requestContext.connectionId;
   const obj = {
@@ -25,49 +39,66 @@ export const connectHandler = async (event, ctx, callback) => {
   };
 };
 
+/**
+ * Disconnect handler
+ *
+ * Cleans up socket table upon graceful disconnect
+ */
 export const disconnectHandler = async (event, ctx, callback) => {
   const connectionID = event.requestContext.connectionId;
-
-  try {
-    const params = {
-      Key: {
-        connectionID
-      },
-      TableName: SOCKETS_TABLE + (process.env.ENVIRONMENT || "")
-    };
-
-    const command = new DeleteCommand(params);
-    const res = await docClient.send(command);
-    return {
-      statusCode: 200,
-      body: res,
-      message: "Disconnected"
-    };
-  } catch (err) {
-    const errorResponse = db.dynamoErrorResponse(err);
-    throw errorResponse;
-  }
+  await deleteConnection(connectionID);
+  return {
+    statusCode: 200,
+    message: "Disconnected"
+  };
 };
 
+/**
+ * Sync action handler
+ * The event.body REQUIRES the following properties:
+ * `{
+ *     id: string
+ * }`
+ *
+ *    to sync connection to admin role and state, set id = "admin"
+ */
 export const syncHandler = async (event, ctx, callback) => {
   const body = JSON.parse(event.body);
   if (!body.hasOwnProperty("id")) {
-    const errMessage = helpers.checkPayloadProps(body, {
+    const errMessage = checkPayloadProps(body, {
       id: {
         required: true,
         type: "string"
       }
     });
-    await helpers.sendMessage(event, errMessage);
+    await sendMessage(event, errMessage);
     return errMessage;
   }
 
-  let state = await helpers.fetchState();
-  const isVoting = state.Item.isVoting;
+  let state = await fetchState();
+  const { isVoting, teamName } = state.Item;
+
+  if (body.id === "admin") {
+    updateSocket(
+      {
+        role: "admin"
+      },
+      event.requestContext.connectionId
+    );
+    await sendMessage(event, {
+      isVoting,
+      teamName,
+      stickers: []
+    });
+    return {
+      statusCode: 200
+    };
+  }
 
   if (!isVoting) {
-    await helpers.sendMessage(event, {
+    await sendMessage(event, {
       isVoting,
+      teamName,
       stickers: []
     });
     return {
@@ -98,13 +129,13 @@ export const syncHandler = async (event, ctx, callback) => {
     };
   } catch (error) {
     db.dynamoErrorResponse(error);
-    await helpers.sendMessage(event, {
+    await sendMessage(event, {
       status: "502",
       message: "Internal server error"
     });
   }
 
-  await helpers.sendMessage(event, {
+  await sendMessage(event, {
     status: 200,
     isVoting,
     stickers
@@ -116,10 +147,10 @@ export const syncHandler = async (event, ctx, callback) => {
 
 /**
  * Admin action handler
- * The event.body must have the following properties:
- * {
- *     event: "start" | "end" | "changeTeam" | "listen"
- * }
+ * The event.body REQUIRES following properties:
+ * `{
+ *     event: "start" | "end" | "changeTeam"
+ * }`
  *
  *    if changeTeam is used as the action, then a team property must
  *    be provided as part of the message body
@@ -127,27 +158,27 @@ export const syncHandler = async (event, ctx, callback) => {
 export const adminHandler = async (event, ctx, callback) => {
   const body = JSON.parse(event.body);
   if (!body.hasOwnProperty("event")) {
-    const errMessage = helpers.checkPayloadProps(body, {
+    const errMessage = checkPayloadProps(body, {
       event: {
         required: true,
         type: "string"
       }
     });
-    await helpers.sendMessage(event, errMessage);
+    await sendMessage(event, errMessage);
     return errMessage;
   }
 
   const action = body.event;
 
   if (action === "changeTeam" && !body.hasOwnProperty("team")) {
-    const errMessage = helpers.checkPayloadProps(body, {
+    const errMessage = checkPayloadProps(body, {
       team: {
         required: true,
         type: "string"
       }
     });
 
-    await helpers.sendMessage(event, errMessage);
+    await sendMessage(event, errMessage);
     return errMessage;
   }
 
@@ -155,87 +186,69 @@ export const adminHandler = async (event, ctx, callback) => {
     let payload;
     switch (action) {
       case "start": {
-        payload = helpers.updateSocket(
+        payload = updateSocket(
           {
             isVoting: true
           },
           "STATE"
         );
-        helpers.sendMessage(event, {
+        await sendMessage(event, {
           status: 200,
           message: payload
         });
-        helpers.notifyVoters(
-          payload,
-          event.requestContext.domainName,
-          event.requestContext.stage
-        );
-        break;
+        await notifyVoters(payload, event);
+        return {
+          statusCode: 200
+        };
       }
 
       case "end": {
-        payload = helpers.updateSocket(
+        payload = updateSocket(
           {
             isVoting: false
           },
           "STATE"
         );
-        helpers.sendMessage(event, {
+        await sendMessage(event, {
           status: 200,
           message: payload
         });
-        helpers.notifyVoters(
-          payload,
-          event.requestContext.domainName,
-          event.requestContext.stage
-        );
-        break;
+        await notifyVoters(payload, event);
+        return {
+          statusCode: 200
+        };
       }
 
       case "changeTeam": {
-        payload = helpers.updateSocket(
+        payload = updateSocket(
           {
             teamName: body.team
           },
           "STATE"
         );
-        helpers.sendMessage(event, {
+        await sendMessage(event, {
           status: 200,
           message: payload
         });
-        helpers.notifyVoters(
-          payload,
-          event.requestContext.domainName,
-          event.requestContext.stage
-        );
-        break;
-      }
-
-      case "listen": {
-        payload = helpers.updateSocket(
-          {
-            role: "admin"
-          },
-          event.requestContext.connectionId
-        );
-        helpers.sendMessage(event, {
-          status: 200,
-          message: payload
-        });
-        break;
+        await notifyVoters(payload, event);
+        return {
+          statusCode: 200
+        };
       }
 
       default: {
-        await helpers.sendMessage(event, {
+        await sendMessage(event, {
           status: "400",
           message: "unrecognized event type"
         });
-        break;
+        return {
+          statusCode: 400
+        };
       }
     }
   } catch (error) {
     console.error(error);
-    await helpers.sendMessage(event, {
+    await sendMessage(event, {
       status: "500",
       message: "Internal Server Error"
     });
@@ -243,19 +256,21 @@ export const adminHandler = async (event, ctx, callback) => {
       statusCode: 500
     };
   }
-  return {
-    statusCode: 200
-  };
 };
 
 export const stickerHandler = async (event, ctx, callback) => {};
 
 export const scoreHandler = async (event, ctx, callback) => {};
 
+/**
+ * Default action handler
+ *
+ * Returns status 400 error for unrecognized action
+ */
 export const defaultHandler = async (event, ctx, callback) => {
   try {
-    await helpers.sendMessage(event, {
-      status: "400",
+    await sendMessage(event, {
+      status: 400,
       message: "unknown action"
     });
   } catch (error) {
--- services/stickers/helpers.js
+++ services/stickers/helpers.js
@@ -5,191 +5,220 @@ import { DeleteCommand, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import docClient from "../../lib/docClient";
 import { RESERVED_WORDS } from "../../constants/dynamodb";
 
-export default {
-  sendMessage: async (event, data) => {
-    const domain = event.requestContext.domainName;
-    const stage = event.requestContext.stage;
-    const connectionId = event.requestContext.connectionId;
-    const url = `https://${domain}/${stage}`;
+/**
+ * @param event socket action event
+ * @param {Object} data message object being sent
+ */
+export const sendMessage = async (event, data) => {
+  const domain = event.requestContext.domainName;
+  const stage = event.requestContext.stage;
+  const connectionId = event.requestContext.connectionId;
+  const url = `https://${domain}/${stage}`;
 
-    try {
-      let apigatewaymanagementapi = new ApiGatewayManagementApi({
-        apiVersion: "2018-11-29",
-        endpoint: process.env.ENVIRONMENT ? url : "http://localhost:3001"
-      });
-      await new Promise((resolve, reject) => {
-        apigatewaymanagementapi.postToConnection(
-          {
-            ConnectionId: connectionId,
-            Data: JSON.stringify(data)
-          },
-          (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(data);
-            }
-          }
-        );
-      });
-    } catch (error) {
-      deleteConnection(connectionId);
-    }
-  },
-
-  fetchState: async () => {
-    let state;
-    try {
-      const command = new GetCommand({
-        TableName: SOCKETS_TABLE,
-        Key: {
-          connectionID: "STATE"
-        }
-      });
-
-      state = await docClient.send(command);
-    } catch (err) {
-      const errorResponse = db.dynamoErrorResponse(err);
-      throw errorResponse;
-    }
-    return state;
-  },
-
-  updateSocket: function (state, connectionID) {
-    let res = {
-      status: 200,
-      message: "Successfully updated state",
-      state: state
-    };
-    try {
-      let {
-        updateExpression,
-        expressionAttributeValues,
-        expressionAttributeNames
-      } = this.createUpdateExpression(state);
-
-      let updateCommand = {
-        Key: {
-          connectionID
-        },
-        TableName:
-          SOCKETS_TABLE +
-          (process.env.ENVIRONMENT ? process.env.ENVIRONMENT : ""),
-        ExpressionAttributeValues: expressionAttributeValues,
-        UpdateExpression: updateExpression,
-        ReturnValues: "UPDATED_NEW",
-        ...(expressionAttributeNames && {
-          ExpressionAttributeNames: expressionAttributeNames
-        })
-      };
-
-      db.updateDBCustom(updateCommand);
-    } catch (error) {
-      console.error(error);
-      res = {
-        status: 500,
-        message: "Internal Server Error"
-      };
-    }
-    return res;
-  },
-
-  notifyVoters: async function (state, domainName, stage) {
-    let voters;
-    try {
-      const command = new QueryCommand({
-        IndexName: "role",
-        ExpressionAttributeNames: {
-          "#role": "role"
-        },
-        ExpressionAttributeValues: {
-          ":role": "voter"
-        },
-        KeyConditionExpression: "#role = :role",
-        ProjectionExpression: "connectionID",
-        TableName: SOCKETS_TABLE
-      });
-      const response = await docClient.send(command);
-      voters = response.Items;
-    } catch (error) {
-      db.dynamoErrorResponse(error);
-    }
-
-    for (let i = 0; i < voters.length; i++) {
-      this.sendMessage(
+  try {
+    let apigatewaymanagementapi = new ApiGatewayManagementApi({
+      apiVersion: "2018-11-29",
+      endpoint: process.env.ENVIRONMENT ? url : "http://localhost:3001"
+    });
+    await new Promise((resolve, reject) => {
+      apigatewaymanagementapi.postToConnection(
         {
-          requestContext: {
-            domainName,
-            stage,
-            connectionId: voters[i].connectionID
-          }
+          ConnectionId: connectionId,
+          Data: JSON.stringify(data)
         },
-        {
-          status: 200,
-          state
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
         }
       );
-    }
-  },
-
-  createUpdateExpression: function (obj) {
-    let val = 0;
-    let updateExpression = "SET ";
-    let expressionAttributeValues = {};
-    let expressionAttributeNames = null;
-
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (RESERVED_WORDS.includes(key.toUpperCase())) {
-          updateExpression += `#v${val} = :val${val},`;
-          expressionAttributeValues[`:val${val}`] = obj[key];
-          if (!expressionAttributeNames) expressionAttributeNames = {};
-          expressionAttributeNames[`#v${val}`] = key;
-          val++;
-        } else {
-          updateExpression += `${key} = :${key},`;
-          expressionAttributeValues[`:${key}`] = obj[key];
-        }
-      }
-    }
-    updateExpression = updateExpression.slice(0, -1);
-
-    return {
-      updateExpression,
-      expressionAttributeValues,
-      expressionAttributeNames
-    };
-  },
-
-  checkPayloadProps: function (payload, check = {}) {
-    try {
-      const criteria = Object.entries(check);
-      criteria.forEach(([key, crit]) => {
-        // check if property exists
-        if (crit.required && !payload[key] && payload[key] !== false) {
-          throw `'${key}' is missing from the request body`;
-        }
-        // check for the property's type
-        if (crit.type && payload[key] && typeof payload[key] !== crit.type) {
-          throw `'${key}' in the request body is invalid, expected type '${
-            crit.type
-          }' but got '${typeof payload[key]}'`;
-        }
-      });
-    } catch (errMsg) {
-      const response = {
-        status: 406,
-        message: errMsg,
-        body:
-          payload && payload.stack && payload.message
-            ? JSON.stringify(payload, Object.getOwnPropertyNames(payload))
-            : JSON.stringify(payload)
-      };
-      return response;
-    }
+    });
+  } catch (error) {
+    deleteConnection(connectionId);
   }
 };
 
+export const fetchState = async () => {
+  let state;
+  try {
+    const command = new GetCommand({
+      TableName: SOCKETS_TABLE,
+      Key: {
+        connectionID: "STATE"
+      }
+    });
+
+    state = await docClient.send(command);
+  } catch (err) {
+    const errorResponse = db.dynamoErrorResponse(err);
+    throw errorResponse;
+  }
+  return state;
+};
+
+/**
+ * @param state socket state object, update role, teamName, isVoting
+ * @param {string} connectionID id of socket connection
+ */
+export function updateSocket(state, connectionID) {
+  let res = {
+    status: 200,
+    message: "Successfully updated state",
+    state: state
+  };
+  try {
+    let {
+      updateExpression,
+      expressionAttributeValues,
+      expressionAttributeNames
+    } = createUpdateExpression(state);
+
+    let updateCommand = {
+      Key: {
+        connectionID
+      },
+      TableName:
+        SOCKETS_TABLE +
+        (process.env.ENVIRONMENT ? process.env.ENVIRONMENT : ""),
+      ExpressionAttributeValues: expressionAttributeValues,
+      UpdateExpression: updateExpression,
+      ReturnValues: "UPDATED_NEW",
+      ...(expressionAttributeNames && {
+        ExpressionAttributeNames: expressionAttributeNames
+      })
+    };
+
+    db.updateDBCustom(updateCommand);
+  } catch (error) {
+    console.error(error);
+    res = {
+      status: 500,
+      message: "Internal Server Error"
+    };
+  }
+  return res;
+}
+
+/**
+ * @param state socket state object, update role, teamName, isVoting
+ * @param {Object} event socket event object
+ */
+export async function notifyVoters(state, event) {
+  let voters;
+  try {
+    const command = new QueryCommand({
+      IndexName: "role",
+      ExpressionAttributeNames: {
+        "#role": "role"
+      },
+      ExpressionAttributeValues: {
+        ":role": "voter"
+      },
+      KeyConditionExpression: "#role = :role",
+      ProjectionExpression: "connectionID",
+      TableName: SOCKETS_TABLE
+    });
+    const response = await docClient.send(command);
+    voters = response.Items;
+  } catch (error) {
+    db.dynamoErrorResponse(error);
+  }
+
+  for (let i = 0; i < voters.length; i++) {
+    sendMessage(
+      {
+        requestContext: {
+          domainName: event.requestContext.domainName,
+          stage: event.requestContext.stage,
+          connectionId: voters[i].connectionID
+        }
+      },
+      {
+        status: 200,
+        state
+      }
+    );
+  }
+}
+
+/**
+ *
+ * @param {*} obj
+ *
+ * returns parsed update expression based on input obj
+ */
+export function createUpdateExpression(obj) {
+  let val = 0;
+  let updateExpression = "SET ";
+  let expressionAttributeValues = {};
+  let expressionAttributeNames = null;
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (RESERVED_WORDS.includes(key.toUpperCase())) {
+        updateExpression += `#v${val} = :val${val},`;
+        expressionAttributeValues[`:val${val}`] = obj[key];
+        if (!expressionAttributeNames) expressionAttributeNames = {};
+        expressionAttributeNames[`#v${val}`] = key;
+        val++;
+      } else {
+        updateExpression += `${key} = :${key},`;
+        expressionAttributeValues[`:${key}`] = obj[key];
+      }
+    }
+  }
+  updateExpression = updateExpression.slice(0, -1);
+
+  return {
+    updateExpression,
+    expressionAttributeValues,
+    expressionAttributeNames
+  };
+}
+
+/**
+ *
+ * @param {Object} payload given body
+ * @param {*} check required elements
+ *
+ * return custom error message
+ */
+export function checkPayloadProps(payload, check = {}) {
+  try {
+    const criteria = Object.entries(check);
+    criteria.forEach(([key, crit]) => {
+      // check if property exists
+      if (crit.required && !payload[key] && payload[key] !== false) {
+        throw `'${key}' is missing from the request body`;
+      }
+      // check for the property's type
+      if (crit.type && payload[key] && typeof payload[key] !== crit.type) {
+        throw `'${key}' in the request body is invalid, expected type '${
+          crit.type
+        }' but got '${typeof payload[key]}'`;
+      }
+    });
+  } catch (errMsg) {
+    const response = {
+      status: 406,
+      message: errMsg,
+      body:
+        payload && payload.stack && payload.message
+          ? JSON.stringify(payload, Object.getOwnPropertyNames(payload))
+          : JSON.stringify(payload)
+    };
+    return response;
+  }
+}
+
+/**
+ * @param {string} connectionID
+ * @returns void
+ *
+ * Deletes connection in sockets table
+ */
 export async function deleteConnection(connectionID) {
   try {
     const params = {
@@ -203,7 +232,6 @@ export async function deleteConnection(connectionID) {
     const res = await docClient.send(command);
     return {
       statusCode: 200,
-      body: res,
       message: "Disconnected"
     };
   } catch (err) {
