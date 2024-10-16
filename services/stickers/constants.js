f117db45f47b6182122b8c635291f7af90417684
Kevin Xiao <kevin.xiao27@gmail.com>
2024-10-15 23:43:42 -0700

    Sticker Service Refactor + Constants (#388)
    * Refactor + removed legacy tests
    
    * Refactor

---
+++ services/stickers/constants.js
@@ -0,0 +1,17 @@
+export const STICKER_TYPE_GOLDEN = "golden";
+export const VOTER_ROLE = "voter";
+export const ADMIN_ROLE = "admin";
+export const STATE_KEY = "STATE";
+export const ACTION_TYPES = {
+  score: "score",
+  state: "state",
+  sticker: "sticker",
+  sync: "sync",
+  error: "error"
+};
+
+export const ADMIN_EVENTS = {
+  start: "start",
+  end: "end",
+  changeTeam: "changeTeam"
+};
