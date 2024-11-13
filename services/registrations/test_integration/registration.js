3dd4f69eaef92d27da6eea58fadbed00df1f1aed
Victor <victorv@ubcbiztech.com>
2024-11-12 22:49:53 -0800

    Partially fix integration tests (running, but not all pass)

--- services/registrations/test_integration/registration.js
+++ services/registrations/test_integration/registration.js
@@ -1,6 +1,8 @@
 "use strict";
-import chai from "chai";
-const expect = chai.expect;
+import {
+  expect
+} from "chai";
+
 import {
   INTEGRATION_TEST_PERSISTENT_USER_EMAIL,
   INTEGRATION_TEST_PERSISTENT_USER_EMAIL_2,
@@ -12,9 +14,9 @@ import {
   INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS,
   INTEGRATION_TEST_PERSISTENT_EVENT_ID_2,
   INTEGRATION_TEST_PERSISTENT_YEAR_2
-} from "../../../constants/test";
+} from "../../../constants/test.js";
 
-import helpers from "../../../lib/testHelpers";
+import helpers from "../../../lib/testHelpers.js";
 
 const SERVICE = "registrations";
 
@@ -27,16 +29,20 @@ describe("registration integration", function () {
         queryStringParameters: {
           year: INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.year,
           email: INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.email,
-          eventID: INTEGRATION_TEST_PERSISTENT_EVENT_ID // wrong eventId
+          eventID: INTEGRATION_TEST_PERSISTENT_EVENT_ID
         }
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationGet", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(200);
-          expect(body.size).to.equal(0);
-          expect(body.data).to.have.length(0);
-        });
+      console.log("invoking registrationGet");
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationGet",
+        JSON.stringify(payload)
+      );
+      console.log(statusCode);
+      console.log(body);
+      expect(statusCode).to.equal(200);
+      expect(body.size).to.equal(0);
+      expect(body.data).to.have.length(0);
     });
 
     it("entry GET event ID query exists returns 200", async () => {
@@ -46,15 +52,16 @@ describe("registration integration", function () {
           year: INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.year
         }
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationGet", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(200);
-          expect(body.size).to.equal(1);
-          expect(body.data[0]["eventID;year"]).to.equal(
-            INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.key
-          );
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationGet",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
+      expect(body.size).to.equal(1);
+      expect(body.data[0]["eventID;year"]).to.equal(
+        INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.key
+      );
     });
 
     it("entry GET by email returns 200", async () => {
@@ -63,13 +70,14 @@ describe("registration integration", function () {
           email: INTEGRATION_TEST_PERSISTENT_USER_EMAIL
         }
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationGet", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(200);
-          expect(body.size).to.be.at.least(1);
-          expect(body.data[0].id).to.equal(INTEGRATION_TEST_PERSISTENT_USER_EMAIL);
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationGet",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
+      expect(body.size).to.be.at.least(1);
+      expect(body.data[0].id).to.equal(INTEGRATION_TEST_PERSISTENT_USER_EMAIL);
     });
   });
 
@@ -81,14 +89,15 @@ describe("registration integration", function () {
         INTEGRATION_TEST_NON_EXISTANT_YEAR,
         "registered"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPost", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(404);
-          expect(body.message).to.equal(
-            `Event with id '${INTEGRATION_TEST_NON_EXISTANT_EVENT_ID}' and secondaryKey '${INTEGRATION_TEST_NON_EXISTANT_YEAR}' could not be found. Make sure you have provided them correctly.`
-          );
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPost",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(404);
+      expect(body.message).to.equal(
+        `Event with id '${INTEGRATION_TEST_NON_EXISTANT_EVENT_ID}' and secondaryKey '${INTEGRATION_TEST_NON_EXISTANT_YEAR}' could not be found. Make sure you have provided them correctly.`
+      );
     });
 
     it("entry POST no such user returns 404", async () => {
@@ -98,14 +107,15 @@ describe("registration integration", function () {
         INTEGRATION_TEST_PERSISTENT_YEAR,
         "registered"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPost", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(404);
-          expect(body.message).to.equal(
-            `User with id '${INTEGRATION_TEST_NON_EXISTANT_USER_EMAIL}' could not be found. Make sure you have provided the correct id.`
-          );
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPost",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(404);
+      expect(body.message).to.equal(
+        `User with id '${INTEGRATION_TEST_NON_EXISTANT_USER_EMAIL}' could not be found. Make sure you have provided the correct id.`
+      );
     });
 
     it("entry POST success returns 201", async () => {
@@ -115,12 +125,13 @@ describe("registration integration", function () {
         INTEGRATION_TEST_PERSISTENT_YEAR,
         "registered"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPost", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(201);
-          expect(body.registrationStatus).to.equal("registered");
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPost",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(201);
+      expect(body.registrationStatus).to.equal("registered");
     });
 
     it("entry POST entry already exists returns 409", async () => {
@@ -130,26 +141,28 @@ describe("registration integration", function () {
         INTEGRATION_TEST_PERSISTENT_YEAR,
         "checkedIn"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPost", JSON.stringify(payload))
-        .then(([statusCode]) => {
-          expect(statusCode).to.equal(409);
-        });
+      const [statusCode] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPost",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(409);
     });
 
-    it("entry POST event at capacity returns 201", async () => {
+    it("entry POST event at capacity returns 201 with waitlist status", async () => {
       const payload = createPayload(
         INTEGRATION_TEST_PERSISTENT_USER_EMAIL_2,
         INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.eventId,
         INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.year,
         "registered"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPost", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(201);
-          expect(body.registrationStatus).to.equal("waitlist");
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPost",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(201);
+      expect(body.registrationStatus).to.equal("waitlist");
     });
   });
 
@@ -161,12 +174,13 @@ describe("registration integration", function () {
         INTEGRATION_TEST_PERSISTENT_YEAR,
         "checkedIn"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPut", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(200);
-          expect(body.registrationStatus).to.equal("checkedIn");
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPut",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
+      expect(body.registrationStatus).to.equal("checkedIn");
     });
 
     it("entry GET to verify put - returns 200", async () => {
@@ -175,27 +189,28 @@ describe("registration integration", function () {
           email: INTEGRATION_TEST_PERSISTENT_USER_EMAIL
         }
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationGet", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(200);
-          expect(body.size).to.equal(2);
-          for (const entry of body.data) {
-            expect(entry.id).to.equal(INTEGRATION_TEST_PERSISTENT_USER_EMAIL);
-            if (
-              entry["eventID;year"] ===
-              `${INTEGRATION_TEST_PERSISTENT_EVENT_ID};${INTEGRATION_TEST_PERSISTENT_YEAR}`
-            ) {
-              expect(entry.registrationStatus).to.equal("checkedIn");
-            }
-            if (
-              entry["eventID;year"] ===
-              `${INTEGRATION_TEST_PERSISTENT_EVENT_ID_2};${INTEGRATION_TEST_PERSISTENT_YEAR_2}`
-            ) {
-              expect(entry.registrationStatus).to.equal("registered");
-            }
-          }
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationGet",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
+      expect(body.size).to.equal(2);
+      for (const entry of body.data) {
+        expect(entry.id).to.equal(INTEGRATION_TEST_PERSISTENT_USER_EMAIL);
+        if (
+          entry["eventID;year"] ===
+          `${INTEGRATION_TEST_PERSISTENT_EVENT_ID};${INTEGRATION_TEST_PERSISTENT_YEAR}`
+        ) {
+          expect(entry.registrationStatus).to.equal("checkedIn");
+        }
+        if (
+          entry["eventID;year"] ===
+          `${INTEGRATION_TEST_PERSISTENT_EVENT_ID_2};${INTEGRATION_TEST_PERSISTENT_YEAR_2}`
+        ) {
+          expect(entry.registrationStatus).to.equal("registered");
+        }
+      }
     });
   });
 
@@ -210,11 +225,12 @@ describe("registration integration", function () {
           year: INTEGRATION_TEST_PERSISTENT_YEAR
         })
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationDelete", JSON.stringify(payload))
-        .then(([statusCode]) => {
-          expect(statusCode).to.equal(200);
-        });
+      const [statusCode] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationDelete",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
     });
 
     it("entry DELETE cleanup returns 200", async () => {
@@ -227,11 +243,12 @@ describe("registration integration", function () {
           year: INTEGRATION_TEST_PERSISTENT_REGISTRATION_PARAMETERS.year
         })
       };
-      return helpers
-        .invokeLambda(SERVICE, "registrationDelete", JSON.stringify(payload))
-        .then(([statusCode]) => {
-          expect(statusCode).to.equal(200);
-        });
+      const [statusCode] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationDelete",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(200);
     });
 
     it("entry PUT no such id and event combination returns 409", async () => {
@@ -241,14 +258,15 @@ describe("registration integration", function () {
         INTEGRATION_TEST_PERSISTENT_YEAR,
         "checkedIn"
       );
-      return helpers
-        .invokeLambda(SERVICE, "registrationPut", JSON.stringify(payload))
-        .then(([statusCode, body]) => {
-          expect(statusCode).to.equal(409);
-          expect(body.message).to.equal(
-            `Update error because the registration entry for user '${INTEGRATION_TEST_PERSISTENT_USER_EMAIL}' and with eventID;year '${INTEGRATION_TEST_PERSISTENT_EVENT_ID};${INTEGRATION_TEST_PERSISTENT_YEAR}' does not exist`
-          );
-        });
+      const [statusCode, body] = await helpers.invokeLambda(
+        SERVICE,
+        "registrationPut",
+        JSON.stringify(payload)
+      );
+      expect(statusCode).to.equal(409);
+      expect(body.message).to.equal(
+        `Update error because the registration entry for user '${INTEGRATION_TEST_PERSISTENT_USER_EMAIL}' and with eventID;year '${INTEGRATION_TEST_PERSISTENT_EVENT_ID};${INTEGRATION_TEST_PERSISTENT_YEAR}' does not exist`
+      );
     });
   });
 });
