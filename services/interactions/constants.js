fad56a553063e878cbf878593e590e6270070fc3
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2025-01-11 21:42:49 -0800

    Fix quest bugs, implement logic for boothing and workshops

--- services/interactions/constants.js
+++ services/interactions/constants.js
@@ -6,10 +6,17 @@ export const PARTNER = "NFC_PARTNER";
 
 export const QUEST_CONNECT_ONE = "QUEST_CONNECT_ONE";
 export const QUEST_SNACK = "QUEST_SNACK";
-export const QUEST_BOOTH_STARTUP = "QUEST_STARTUP";
+export const QUEST_STARTUP = "QUEST_STARTUP";
 export const QUEST_BIGTECH = "QUEST_BIGTECH";
+export const QUEST_WORKSHOP = "QUEST_WORKSHOP";
 export const QUEST_PHOTOBOOTH = "QUEST_PHOTOBOOTH";
 export const QUEST_CONNECT_FOUR = "QUEST_CONNECT_FOUR";
 export const QUEST_BT_BOOTH_H = "QUEST_BT_BOOTH_H";
 export const QUEST_CONNECT_TEN_H = "QUEST_CONNECT_TEN_H";
 export const QUEST_CONNECT_EXEC_H = "QUEST_CONNECT_EXEC_H";
+
+export const WORKSHOP_TWO = "WORKSHOP_TWO";
+
+export const BIGTECH = ["Meta", "Google"]; // incomplete, check with partnerships
+export const STARTUPS = ["Perplexity", "InternInside", "Xpress"];
+export const PHOTOBOOTH = "PHOTO";
--- services/interactions/helpers.js
+++ services/interactions/helpers.js
@@ -1,17 +1,38 @@
+import { QueryCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import {
-  QueryCommand
-} from "@aws-sdk/lib-dynamodb";
-import {
-  QRS_TABLE, CONNECTIONS_TABLE
+  QRS_TABLE,
+  CONNECTIONS_TABLE,
+  QUESTS_TABLE
 } from "../../constants/tables";
 import db from "../../lib/db";
 import handlerHelpers from "../../lib/handlerHelpers";
 import docClient from "../../lib/docClient";
 import {
-  ATTENDEE, CURRENT_EVENT, EXEC, PARTNER
+  ATTENDEE,
+  BIGTECH,
+  CURRENT_EVENT,
+  EXEC,
+  PARTNER,
+  QUEST_BIGTECH,
+  QUEST_STARTUP,
+  QUEST_CONNECT_FOUR,
+  QUEST_CONNECT_ONE,
+  QUEST_CONNECT_TEN_H,
+  QUEST_WORKSHOP,
+  STARTUPS,
+  WORKSHOP_TWO,
+  PHOTOBOOTH,
+  QUEST_PHOTOBOOTH,
+  QUEST_CONNECT_EXEC_H
 } from "./constants";
 
 export const handleConnection = async (userID, connID, timestamp) => {
+  if (userID == connID) {
+    return handlerHelpers.createResponse(400, {
+      message: "Cannot connect with yourself"
+    });
+  }
+
   let profileData = await db.getOne(userID, QRS_TABLE, {
     "eventID;year": CURRENT_EVENT
   });
@@ -27,12 +48,8 @@ export const handleConnection = async (userID, connID, timestamp) => {
     });
   }
 
-  const {
-    data: userData
-  } = profileData;
-  const {
-    data: connData, type
-  } = connProfileData;
+  let { data: userData, type: userType } = profileData;
+  let { data: connData, type: connType } = connProfileData;
 
   if (
     await isDuplicateRequest(userData.registrationID, connData.registrationID)
@@ -42,134 +59,176 @@ export const handleConnection = async (userID, connID, timestamp) => {
     });
   }
 
+  let swap = false;
+  if (userType == EXEC && connType == EXEC) {
+    connType = EXEC + EXEC;
+  } else if (userType == EXEC) {
+    // in the case that the first user is an EXEC, switch required for switch-case logic to catch
+    userData = [connData, (connData = userData)][0];
+    userType = [connType, (connType = userType)][0];
+    userID = [connID, (connID = userID)][0];
+    swap = true;
+  }
+
   const userPut = {
     userID: userData.registrationID,
-    connID: connData.registrationID,
     obfuscatedID: connID,
     "eventID;year": CURRENT_EVENT,
     createdAt: timestamp,
     ...(connData.linkedinURL
       ? {
-        linkedinURL: connData.linkedinURL
-      }
-      : {
-      }),
+          linkedinURL: connData.linkedinURL
+        }
+      : {}),
     ...(connData.fname
       ? {
-        fname: connData.fname
-      }
-      : {
-      }),
+          fname: connData.fname
+        }
+      : {}),
     ...(connData.lname
       ? {
-        lname: connData.lname
-      }
-      : {
-      }),
+          lname: connData.lname
+        }
+      : {}),
     ...(connData.major
       ? {
-        major: connData.major
-      }
-      : {
-      }),
+          major: connData.major
+        }
+      : {}),
     ...(connData.year
       ? {
-        year: connData.year
-      }
-      : {
-      }),
+          year: connData.year
+        }
+      : {}),
     ...(connData.company
       ? {
-        company: connData.company
-      }
-      : {
-      }),
+          company: connData.company
+        }
+      : {}),
     ...(connData.title
       ? {
-        title: connData.title
-      }
-      : {
-      })
+          title: connData.title
+        }
+      : {})
   };
 
   const connPut = {
     userID: connData.registrationID,
-    connID: userData.registrationID,
     obfuscatedID: userID,
     "eventID;year": CURRENT_EVENT,
     createdAt: timestamp,
     ...(userData.linkedinURL
       ? {
-        linkedinURL: userData.linkedinURL
-      }
-      : {
-      }),
+          linkedinURL: userData.linkedinURL
+        }
+      : {}),
     ...(userData.fname
       ? {
-        fname: userData.fname
-      }
-      : {
-      }),
+          fname: userData.fname
+        }
+      : {}),
     ...(userData.lname
       ? {
-        lname: userData.lname
-      }
-      : {
-      }),
+          lname: userData.lname
+        }
+      : {}),
     ...(userData.major
       ? {
-        major: userData.major
-      }
-      : {
-      }),
+          major: userData.major
+        }
+      : {}),
     ...(userData.year
       ? {
-        year: userData.year
-      }
-      : {
-      }),
+          year: userData.year
+        }
+      : {}),
     ...(userData.company
       ? {
-        company: userData.company
-      }
-      : {
-      }),
+          company: userData.company
+        }
+      : {}),
     ...(userData.title
       ? {
-        title: userData.title
-      }
-      : {
-      })
+          title: userData.title
+        }
+      : {})
   };
 
-  switch (type) {
-  case ATTENDEE:
-    try {
-      // potential race condition -> use transactions to fix, but will take time to implement
-      await db.put(connPut, CONNECTIONS_TABLE, true);
-      await db.put(userPut, CONNECTIONS_TABLE, true);
-      // logic to check if quests entry has been made for connection
-      // put command to update the quest entry
-    } catch (error) {
-      console.error(error);
-      return handlerHelpers.createResponse(500, {
-        message: "Internal server error"
-      });
-    }
-    break;
+  switch (connType) {
+    case EXEC + EXEC:
+      try {
+        await incrementQuestProgress(
+          connData.registrationID,
+          QUEST_CONNECT_EXEC_H
+        );
+      } catch (error) {
+        console.error(error);
+        return handlerHelpers.createResponse(500, {
+          message: "Internal server error"
+        });
+      }
 
-  case EXEC:
-    break;
+    case EXEC:
+      try {
+        await incrementQuestProgress(
+          userData.registrationID,
+          QUEST_CONNECT_EXEC_H
+        );
+      } catch (error) {
+        console.error(error);
+        return handlerHelpers.createResponse(500, {
+          message:
+            "Internal server error, data inconsistency :( not all quests progressed"
+        });
+      }
 
-  case PARTNER:
-    break;
+    // case ATTENDEE:
+    default:
+      try {
+        // potential race condition -> use transactions to fix, but will take time to implement
+        await db.put(connPut, CONNECTIONS_TABLE, true);
+        await db.put(userPut, CONNECTIONS_TABLE, true);
+        await incrementQuestProgress(
+          userData.registrationID,
+          QUEST_CONNECT_ONE
+        );
+        await incrementQuestProgress(
+          userData.registrationID,
+          QUEST_CONNECT_FOUR
+        );
+        await incrementQuestProgress(
+          userData.registrationID,
+          QUEST_CONNECT_TEN_H
+        );
+        await incrementQuestProgress(
+          connData.registrationID,
+          QUEST_CONNECT_ONE
+        );
+        await incrementQuestProgress(
+          connData.registrationID,
+          QUEST_CONNECT_FOUR
+        );
+        await incrementQuestProgress(
+          connData.registrationID,
+          QUEST_CONNECT_TEN_H
+        );
+      } catch (error) {
+        console.error(error);
+        return handlerHelpers.createResponse(500, {
+          message: "Internal server error"
+        });
+      }
+      break;
 
-  default:
-    break;
+    // case PARTNER:
+    //   break;
+
+    // default:
+    //   break;
   }
 
   return handlerHelpers.createResponse(200, {
-    message: `Connection created with ${connData.registrationID}`
+    message: `Connection created with ${swap ? userData.fname : connData.fname}`
   });
 };
 
@@ -181,22 +240,101 @@ const isDuplicateRequest = async (userID, connID) => {
         ":conn": connID,
         ":uid": userID
       },
-      KeyConditionExpression: "connID = :conn AND userID = :uid",
-      ProjectionExpression: "userID, connID",
+      KeyConditionExpression: "obfuscatedID = :conn AND userID = :uid",
+      ProjectionExpression: "userID, obfuscatedID",
       TableName: CONNECTIONS_TABLE + (process.env.ENVIRONMENT || "")
     });
 
     result = await docClient.send(command);
   } catch (error) {
     console.error(error);
-    throw handlerHelpers.createResponse(500, {
-      message: "dynamodb silly"
-    });
+    throw error;
   }
 
   return result.Items.length > 0;
 };
 
-export const handleWorkshop = async (userID, workshopID, timestamp) => {};
+export const handleWorkshop = async (profileID, workshopID, timestamp) => {
+  const { data } = await db.getOne(profileID, QRS_TABLE, {
+    "eventID;year": CURRENT_EVENT
+  });
 
-export const handleBooth = async (userID, boothID, timestamp) => {};
+  let userID = data.registrationID;
+
+  if (workshopID != WORKSHOP_TWO) {
+    return handlerHelpers.createResponse(200, {
+      message: `Checked into ${workshopID}`
+    });
+  }
+
+  try {
+    await incrementQuestProgress(userID, QUEST_WORKSHOP);
+    return handlerHelpers.createResponse(200, {
+      message: "Checked into workshop 2"
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const handleBooth = async (profileID, boothID, timestamp) => {
+  const { data } = await db.getOne(profileID, QRS_TABLE, {
+    "eventID;year": CURRENT_EVENT
+  });
+
+  let userID = data.registrationID;
+
+  if (BIGTECH.includes(boothID)) {
+    try {
+      await incrementQuestProgress(userID, QUEST_BIGTECH);
+      return handlerHelpers.createResponse(200, {
+        message: `Checked into booth ${boothID}`
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  if (STARTUPS.includes(boothID)) {
+    try {
+      await incrementQuestProgress(userID, QUEST_STARTUP);
+      return handlerHelpers.createResponse(200, {
+        message: `Checked into booth ${boothID}`
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  if (boothID == PHOTOBOOTH) {
+    try {
+      await incrementQuestProgress(userID, QUEST_PHOTOBOOTH);
+      return handlerHelpers.createResponse(200, {
+        message: `Checked into booth ${boothID}`
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+};
+
+const incrementQuestProgress = async (userID, questID) => {
+  const command = new UpdateCommand({
+    TableName: QUESTS_TABLE + (process.env.ENVIRONMENT || ""),
+    Key: {
+      userID,
+      questID
+    },
+    UpdateExpression: "ADD progress :incrementValue",
+    ExpressionAttributeValues: {
+      ":incrementValue": 1
+    },
+    ReturnValues: "ALL_NEW"
+  });
+
+  return await docClient.send(command);
+};
