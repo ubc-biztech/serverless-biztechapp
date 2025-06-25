8848d51761926d97c9c5e1d9c587a985d3a0b5b3
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2025-06-25 12:03:00 -0700

    infra + helpers

---
+++ services/bots/helpersDiscord.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+
+export async function DiscordRequest(endpoint, options) {
+  const url = "https://discord.com/api/v10/" + endpoint;
+  if (options.body) options.body = JSON.stringify(options.body);
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      "Content-Type": "application/json; charset=UTF-8",
+      "User-Agent":
+        "DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)"
+    },
+    ...options
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
+  }
+
+  return res;
+}
