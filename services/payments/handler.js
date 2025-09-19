9a409de772c934ef47aa137fd9064922ba431a88
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-09-19 09:04:44 -0700

    Test remove comment

--- services/payments/handler.js
+++ services/payments/handler.js
@@ -345,7 +345,6 @@ export const webhook = async (event, ctx, callback) => {
 };
 
 export const payment = async (event, ctx, callback) => {
-  // handles payment for event or membership
   try {
     let data = JSON.parse(event.body);
     if (data.email) {
