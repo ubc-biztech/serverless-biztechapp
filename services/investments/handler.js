0eda13a281573d0648ebe6db9a60203359dc2195
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-10-16 09:25:18 -0700

    Init investments service

---
+++ services/investments/handler.js
@@ -0,0 +1,27 @@
+// TODO
+export const invest = async (event, ctx, callback) => {
+    /*
+    Responsible for:
+    - Decrementing the balance of the investor
+    - Incrementing the balance of the team
+    - Updating the DB with transaction + comments
+    */
+}
+
+// TODO
+export const userStatus = async (event, ctx, callback) => {
+    /*
+    Responsible for:
+    - Fetching user current balance
+    - Fetching user's stake in other teams
+    */
+}
+
+// TODO
+export const teamStatus = async (event, ctx, callback) => {
+    /*
+    Responsible for:
+    - Fetching team's current funding
+    */
+} 
+    
