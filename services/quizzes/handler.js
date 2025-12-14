272d933ae1412bc001f98c465fbcd9ed363df4cf
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-12-13 19:28:52 -0800

    Quizzes Service for BP

---
+++ services/quizzes/handler.js
@@ -0,0 +1,13 @@
+import helpers from "../../lib/handlerHelpers.js";
+
+export const upload = async (event, ctx, callback) => {
+    return helpers.createResponse(200, {
+        message: "Upload successful"
+    });  
+}
+
+export const report = async (event, ctx, callback) => {
+    return helpers.createResponse(200, {
+        message: "Report generated"
+    });  
+}
