fb3a0dd0fa2544988355060f1fea4e02dd247327
liuisaac <80558145+liuisaac@users.noreply.github.com>
2026-04-29 12:55:12 -0700

    cleanup dist files

--- dist/constants/indexes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OPENSEARCH_INDEX_TOP_K = exports.BLUEPRINT_OPENSEARCH_PROD_INDEX = exports.BLUEPRINT_OPENSEARCH_STAGING_INDEX = exports.BLUEPRINT_OPENSEARCH_TEST_INDEX = void 0;
-exports.BLUEPRINT_OPENSEARCH_TEST_INDEX = "test-index";
-exports.BLUEPRINT_OPENSEARCH_STAGING_INDEX = "blueprint-staging";
-exports.BLUEPRINT_OPENSEARCH_PROD_INDEX = "blueprint-prod";
-exports.OPENSEARCH_INDEX_TOP_K = 5;
--- dist/lib/sesV2Client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sesClient = void 0;
-const client_sesv2_1 = require("@aws-sdk/client-sesv2");
-exports.sesClient = new client_sesv2_1.SESv2Client({
-    region: "us-west-2",
-});
